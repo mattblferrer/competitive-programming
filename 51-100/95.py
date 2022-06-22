aa0ed2fc64179a4da6a3f0483a654f8cc6c997df
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-06-22 15:03:47 +0800

    solved problem 95
    + updated gitignore

--- 51-100/95.py
+++ 51-100/95.py
@@ -10,11 +10,47 @@ def factors(num):
     return factor_sum
 
 
+# determines if num is prime
+def isprime(num):
+    for i in range(2, int(num**0.5+1)):  # for odd numbers
+        if num % i == 0:
+            return False
+    return True
+
+
 # declare variables
-n = 15472  # given
+n = 15473  # given
 limit = 1000000
 max_chain_length = 5  # given
+max_chain = []  # stores the longest possible amicable chain
+output = 0  # smallest element of the longest amicable chain
 
 # main loop
-# TODO
+while n < limit:
+    test_n = factors(n)  # this will be the chain variable
+    current_chain = [n]  # the current n will be initialized in the chain
+    isChain = False
 
+    while test_n != 1:  # if 1, then it hit a prime number
+        if test_n < limit:  # if it exceeded 1 million
+            if test_n == n:  # if chain looped back to the start
+                # check for chain length
+                if len(current_chain) > max_chain_length:
+                    max_chain_length = len(current_chain)
+                    max_chain = current_chain
+                    output = min(current_chain)
+
+            if test_n not in current_chain:
+                current_chain.append(test_n)
+                test_n = factors(test_n)
+            else:
+                break
+        else:
+            break
+    n += 1
+
+# print output
+print("The smallest member of the longest amicable chain with no element exceeding {} is {}"
+      .format(limit, output))
+print("Longest chain: ", max_chain)
+print("Length of longest chain: ", max_chain_length)
