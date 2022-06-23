988e4e847a3ec59e7b2976639b141ae19770b1cd
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-06-23 12:42:42 +0800

    solved problem 69
    + updated problem 3

--- 1-50/3.py
+++ 1-50/3.py
@@ -1,10 +1,18 @@
+# returns the prime factors of num
+def prime_factorize(num):
+    factors = []
+
+    while num % 2 == 0:  # for only even prime
+        factors.append(2)
+        num //= 2
+
+    for i in range(3, int(num ** 0.5) + 1, 2):  # only have to check for odd factors up to sqrt(n)
+        while num % i == 0:
+            factors.append(i)
+            num //= i
+
+    return factors
+
+
 n = 600851475143  # given n
-sqrtn = int(n**0.5)
-primeFactors = []
-
-for i in range(2, sqrtn):  # only have to check for factors below sqrt(n)
-    if n % i == 0:
-        primeFactors.append(i)
-        n //= i
-
-print(primeFactors)
+print(prime_factorize(n))
---
+++ 51-100/69.py
@@ -0,0 +1,43 @@
+# returns the prime factors of num (non-repeating)
+def prime_factorize(num):
+    factors = set()
+
+    while num % 2 == 0:  # for only even prime
+        factors.add(2)
+        num //= 2
+
+    for i in range(3, int(num ** 0.5) + 1, 2):  # only have to check for odd factors up to sqrt(n)
+        while num % i == 0:
+            factors.add(i)
+            num //= i
+
+    return factors
+
+
+# returns the Euler's totient of the number num using Euler's product formula
+def totient(num):
+    p_factors = prime_factorize(num)
+    phi = num  # totient
+    for factor in p_factors:
+        phi = phi * (factor-1)//factor
+
+    return phi
+
+
+# declare variables
+limit = 1000000
+maximumRatio = 0
+maximumN = 0
+
+# main loop
+for n in range(1, limit+1):
+    ratio = n/totient(n)
+
+    # compare with maximum ratio
+    if ratio > maximumRatio:
+        maximumRatio = ratio
+        maximumN = n
+
+# print output
+print("The value of n for which n/phi(n) is at a maximum is {}".format(maximumN))
+print("The maximum value of n/phi(n) is {}".format(maximumRatio))
