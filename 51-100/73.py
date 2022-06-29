592806986ba22bc4b79637814c94274ba34807eb
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-06-29 19:30:34 +0800

    solved problem 73

--- 51-100/73.py
+++ 51-100/73.py
@@ -1,1 +1,18 @@
-# TODO
+from math import gcd, ceil
+
+
+# declare variables
+limit = 12000
+counter = 0
+
+# main loop
+for denominator in range(5, limit+1):
+    lowerLimit = ceil(denominator / 3)
+    upperLimit = ceil(denominator / 2)
+
+    for numerator in range(lowerLimit, upperLimit):
+        if gcd(numerator, denominator) == 1:
+            counter += 1
+
+# print result
+print("The number of fractions between 1/3 and 1/2 is", counter)
