4107de0f4950175c133f1852ec1ec256fb905d5f
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-06-29 18:54:36 +0800

    solved problem 74

--- 51-100/74.py
+++ 51-100/74.py
@@ -3,7 +3,7 @@ def fac_digit_sum(num):
     factorial_list = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]  # pre-computed list of all factorials 0-9
     factorial_sum = 0
 
-    digits = [int(digit) for digit in str(i)]
+    digits = [int(digit) for digit in str(num)]
 
     # take factorials of digits
     for digit in digits:
@@ -12,4 +12,34 @@ def fac_digit_sum(num):
     return factorial_sum
 
 
-# TODO main function
+# returns the length of the factorial sum chain of num
+def fac_digit_chain(num):
+    current_chain = []
+    counter = 0
+    current_sum = num
+
+    while current_sum not in current_chain:
+        current_chain.append(current_sum)
+        current_sum = fac_digit_sum(current_sum)
+        counter += 1
+
+    return counter
+
+
+# declare variables
+limit = 1000000
+chainLength = [0]*1000000
+answer = 0
+
+# main loop
+for i in range(limit):
+    if fac_digit_chain(i) == 60:
+        answer += 1
+
+# print result
+print("The number of chains that have 60 non-repeating terms:", answer)
+
+
+
+
+
