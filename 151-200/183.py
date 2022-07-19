c744ebf67e1c344e0d0358ad3d3eb6f3db41c55f
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-07-19 10:51:11 +0800

    solved problem 183

---
+++ 151-200/183.py
@@ -0,0 +1,52 @@
+from math import e, gcd
+
+
+# reduces fraction to its lowest terms
+def reduce(numerator, denominator):
+    # divide numerator, denominator by their GCD
+    frac_gcd = gcd(numerator, denominator)
+    numerator //= frac_gcd
+    denominator //= frac_gcd
+
+    return numerator, denominator
+
+
+# returns True if fraction's decimal expansion terminates, and False otherwise
+def is_terminating(numerator, denominator):
+    while denominator % 2 == 0:
+        denominator //= 2
+
+    while denominator % 5 == 0:
+        denominator //= 5
+
+    if denominator == 1:
+        return True
+    return False
+
+
+# declare variables
+limit = 10000
+i = 5
+Dn = 0
+
+# main loop
+while i <= limit:
+    maxParts = round(i/e)
+    reducedFrac = reduce(i, maxParts)
+    n = reducedFrac[0]  # numerator
+    d = reducedFrac[1]  # denominator
+    
+    # check if max parts evenly divide the number into integer parts
+    if n == 1:
+        Dn -= i
+
+    # check if decimal expansion is terminating
+    elif is_terminating(n, d):
+        Dn -= i
+    else:
+        Dn += i
+
+    i += 1
+
+# print result
+print("The sum of D(n) =", Dn)
