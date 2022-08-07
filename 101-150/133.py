58cf72ee9253d317bad2f0a4fc848f957c7c3fce
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-08-07 17:28:08 +0800

    removed unneeded function

--- 101-150/133.py
+++ 101-150/133.py
@@ -40,33 +40,6 @@ def soe(n):
             multiple += (i+2)
 
 
-# returns the prime factors of num (non-repeating)
-def prime_factorize(num):
-    factors = set()
-
-    while num % 2 == 0:  # for only even prime
-        factors.add(2)
-        num //= 2
-
-    while num % 3 == 0:  # for 3
-        factors.add(3)
-        num //= 3
-
-    for i in range(6, int(num ** 0.5) + 3, 6):  # for 6k +- 1
-        while num % (i-1) == 0:
-            factors.add(i-1)
-            num //= (i-1)
-            
-        while num % (i+1) == 0:
-            factors.add(i+1)
-            num //= (i+1)
-
-    if num != 1:
-        factors.add(num)
-
-    return factors
-
-
 # create a sieve of Eratosthenes
 limit = 100000
 iterlimit = int(sqrt(limit)) + 1
