438a092e5321cdf134d2d8004e8126be30205c4a
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-09-23 12:01:19 +0800

    improved 119

--- 101-150/119.py
+++ 101-150/119.py
@@ -1,56 +1,39 @@
-from math import log, sqrt
+from math import log
 
 
 # returns the sum of the digits of num
-def digit_sum(num):
+def digit_sum(num: int) -> int:
     return sum([int(digit) for digit in str(num)])
 
 
-# return next perfect power
-def next_power(num):
-    possible_powers = set()
-    high_exp = int(log(num, 2))+1  # highest exponent for next power
-
-    # only have to check 2, odd exponents
-    base = int(sqrt(num))+1
-    power = base*base
-    
-    if power > num:
-        possible_powers.add(power)
-
-    # odd exponents
-    for exponent in range(3, high_exp, 2):
-        base = int(num ** (1/exponent))+1  # next base up
-        power = base**exponent
-
-        if power > num:
-            possible_powers.add(power)
-
-    return min(possible_powers)
-
-
 # checks if num is a perfect power of a base
-def is_power(num, base):
-    high_exp = int(log(num, base))+2 # highest exponent for next power
+def is_power(num: int, base: int) -> bool:
+    if base == 1:
+        return False
 
-    for exponent in range(high_exp - 2, high_exp):
-        if base**exponent == num:
-            return True
-
+    exp = int(round(log(num, base)))  # round log to nearest whole number
+    if base ** exp == num:  # check if log is an integer
+        return True
     return False
 
 
-# declare variables
-index = 1
-n = 10  # initial value of power
+# generate i^p up to reasonable limit
+powers = set()
+limit = 100
 
-# main loop
-while True:
-    n = next_power(n)
-    digitSum = digit_sum(n)
+for i in range(1, limit):
+    for p in range(1, limit):
+        powers.add(i**p)
 
-    if digitSum > 1:
-        if is_power(n, digit_sum(n)):
-            print(index, n)
-            index += 1
-    
+powers = sorted(list(powers))
+
+# iterate through powers
+a = 1
+
+for num in powers:
+    if num > 10 and is_power(num, digit_sum(num)):
+        print(a, num)
+        a += 1
+
+    if a > 30:
+        break
