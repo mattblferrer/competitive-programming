7973dad8f219b6ba1675ae7b62fd8667d440e792
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-12-10 20:29:12 +0800

    updated 66, 95, 182

--- 051-100/066.py
+++ 051-100/066.py
@@ -5,10 +5,9 @@ from math import sqrt
 
 
 # returns x in the equation x^2 - Dy^2 = 1 for a given D
-def diophantine_solve(d):
+def diophantine_solve(d: int) -> int:
     # starting values of x and y
-    y = 1
-    x = int(sqrt(d)) + 1
+    x, y = int(sqrt(d)) + 1, 1
 
     while True:
         # substitute into equation to get x^2 - Dy^2 = k to get k
@@ -18,32 +17,36 @@ def diophantine_solve(d):
         if k == 1:
             return x
 
-        else:
-            # finding next value of m for triple composition
-            minimal_sqr_diff = float('inf')
-            for i in range(int(sqrt(d))+2):
-                if (x+y*i) % k == 0:
-                    if abs(i*i - d) < minimal_sqr_diff:
-                        minimal_sqr_diff = abs(i*i - d)
-                        m = i
+        # finding next value of m for triple composition
+        minimal_sqr_diff = float('inf')
+        for i in range(int(sqrt(d))+2):
+            if (x+y*i) % k == 0 and abs(i*i - d) < minimal_sqr_diff:
+                minimal_sqr_diff = abs(i*i - d)
+                m = i
 
-            # new triple composition
-            x, y, k = (x*m + d*y)//abs(k), (x + m*y)//abs(k), (m*m - d)//k
+        # new triple composition
+        x, y, k = (x*m + d*y)//abs(k), (x + m*y)//abs(k), (m*m - d)//k
 
 
-# declare variables
-limit = 1000
-maximumD = 0
-maximumX = 0
+def main():
+    # declare variables
+    limit = 1000
+    maximum_d = 0
+    maximum_x = 0
 
-# main loop
-for i in range(1, limit+1):
-    if not sqrt(i).is_integer():  # perfect square check
-        ans = diophantine_solve(i)
-        if maximumX < ans:
-            maximumX = ans
-            maximumD = i
+    # main loop
+    for i in range(1, limit + 1):
+        if not sqrt(i).is_integer():  # perfect square check
+            ans = diophantine_solve(i)
+            if maximum_x < ans:
+                maximum_x = ans
+                maximum_d = i
 
-# print result
-print("The value of D for which the largest value of x is obtained is", maximumD)
-print("The largest value of x is", maximumX)
+    # print result
+    print("The value of D for which the largest value of x is "\
+        f"obtained is {maximum_d}")
+    print(f"The largest value of x is {maximum_x}")
+
+
+if __name__ == "__main__":
+    main()
