22ef9a8faca4b6bdca7ff175bb24e3f7bf938292
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-12-11 19:33:02 +0800

    updated 101, 122, 288

--- 101-150/122.py
+++ 101-150/122.py
@@ -1,45 +1,47 @@
 # returns the multiplication chain with n multiplied by chain[index]
-def mult_exp_chain(chain: list, index: int, limit: int) -> list:
-    new_chain = chain.copy()
+def mult_exp_chain(chain: list, index: int, limit: int) -> list[int]:
+    new_chain = None
     new_exp = chain[-1] + index
 
     if new_exp <= limit:
+        new_chain = chain.copy()
         new_chain.append(new_exp)
 
     return new_chain
 
 
-# returns the array of chain multiplications with limit
+# returns the array of chain multiplications below limit
 def mult_chain(chains: list, limit: int) -> list:
+    # initialize chain length array for checking 
+    chain_length = [float('inf')] * (limit + 1)
+    chain_length[1] = 1
+
     for index, n in enumerate(chains):  # index -> maximum exponent 
-        if n:   # check if n has chains
-            # generate new chains
-            for chain in n:
-                for i in chain:
-                    new_chain = mult_exp_chain(chain, i, limit)
-                    max_exp = new_chain[-1]  # maximum exponent reached by new chain
-                    
-                    # check if a chain does not exist
-                    if not chains[max_exp]: 
-                        chains[max_exp] = [new_chain]
-                        
-                    # if a chain exists, then...
-                    else:
-                        for check_chain in chains[max_exp]:
-                            new_chain_length = len(new_chain)
-                            check_chain_length = len(check_chain)
+        if not n:   # check if n has chains
+            continue 
 
-                            # check if chain generated is more efficient
-                            if new_chain_length < check_chain_length:
-                                chains[max_exp] = [new_chain]
+        # generate new chains
+        for chain in n:
+            for i in chain:
+                new_chain = mult_exp_chain(chain, i, limit)
+                if not new_chain:  # if no new chain can be made
+                    continue
 
-                            # if chain is just as efficient, add choice to list
-                            if new_chain_length == check_chain_length and new_chain not in chains[max_exp]:
-                                chains[max_exp].append(new_chain)
-                                break
+                max_exp = new_chain[-1]  # maximum exponent reached by chain
+                new_chain_length = len(new_chain)
+                check_chain_length = chain_length[max_exp]
+                
+                # check if a chain does not exist or if chain is more efficient
+                if not chains[max_exp] or new_chain_length < check_chain_length: 
+                    chains[max_exp] = [new_chain]
+                    chain_length[max_exp] = new_chain_length
 
-            ways = len(n)  # how many minimum chains there are for n 
-            print(index, ways, n[0])  # track progress of function
+                # if chain is just as efficient, add choice to list
+                elif new_chain_length == check_chain_length:
+                    chains[max_exp].append(new_chain)
+
+        ways = len(n)  # how many minimum chains there are for n 
+        print(index, ways, chain)  # track progress of function
 
     return chains  # return list of minimum chains for each k below limit
  
@@ -47,7 +49,8 @@ def mult_chain(chains: list, limit: int) -> list:
 def main():
     # declare variables
     k = 200
-    min_mults = [[] for _ in range(k + 1)]  # list of minimum multiplication chains
+    # list of minimum multiplication chains
+    min_mults = [[] for _ in range(k + 1)]  
 
     # calculate all possible minimum chains of multiplications to get to n^k
     min_mults[1] = [[1]]  # n^1 (only one way)
@@ -56,11 +59,10 @@ def main():
     # calculate sum of all m(k) below limit k
     min_mults = [chains[0] for chains in min_mults]
     mk = [len(chain) - 1 for chain in min_mults]
-    sum_mk = sum(mk)
 
     # print result
-    print(f"Sum of m(k): {sum_mk}")
+    print(f"Sum of m(k): {sum(mk)}")
 
 
 if __name__ == "__main__":
-    main()
+   main()
--- 251-300/288.py
+++ 251-300/288.py
@@ -1,22 +1,26 @@
-# declare variables
-p = 61
-q = 10**7
-exp = 10  # exponent of modulus
+def main():
+    # declare variables
+    p, q = 61, 10**7
+    exp = 10  # exponent of modulus
 
-# random number generator
-s = 290797  # s0
-n = 0
-power = p  # prime power multiple
+    # random number generator
+    s = 290_797  # s0
+    n = 0
+    power = p  # prime power multiple
 
-for i in range(1, q+1):  # exponent of modulus
-    s = (s*s) % 50515093  # generate S(n)
-    t = s % p  # generate T(n)
-    
-    # calculate NF(p,q)
-    n += t*(power - 1)//(p-1)  # factors in (T(n)*p^n)!
+    for i in range(1, q + 1):  # exponent of modulus
+        s = s * s % 50_515_093  # generate S(n)
+        t = s % p  # generate T(n)
+        
+        # calculate NF(p,q)
+        n += t*(power - 1)//(p - 1)  # factors in (T(n)*p^n)!
 
-    if i < exp:  # increment power when lower than modulus
-        power *= p
+        if i < exp:  # increment power when lower than modulus
+            power *= p
 
-# print result
-print(n % (p**exp)) 
+    # print result
+    print(n % power) 
+
+
+if __name__ == "__main__":
+    main()
