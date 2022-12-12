f3749064492c9a4da4b2e5cce65c02014e4d660d
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-12-12 15:49:31 +0800

    updated 132, 133

--- 101-150/133.py
+++ 101-150/133.py
@@ -4,20 +4,20 @@ from math import log2, sqrt
 # creates a Sieve of Eratosthenes array of size n
 def soe(n: int) -> list:
     iterlimit = int(sqrt(n)) + 1
-    isPrimeList = [True]*(n+1)
+    isPrimeList = [True]*n
 
     # for 0 and 1 
     isPrimeList[0] = isPrimeList[1] = False
 
     # for 2 and 3
-    for i in [2, 3]:
+    for i in (2, 3):
         for multiple in range(i*i, n, i):
             # assign multiples of 2 or 3 as not being prime
             isPrimeList[multiple] = False  
 
     # for 6k +- 1
     for i in range(5, iterlimit+2, 6): 
-        for j in [0, 2]: 
+        for j in (0, 2): 
             for multiple in range((i+j) * (i+j), n, i+j):
                 # assign multiples of i+j as not being prime
                 isPrimeList[multiple] = False  
@@ -25,27 +25,30 @@ def soe(n: int) -> list:
     return isPrimeList
 
 
-# create a sieve of Eratosthenes
-limit = 100000
-isPrimeList = soe(limit)
+def main():
+    # create a sieve of Eratosthenes
+    limit = 100_000
+    is_prime_list = soe(limit)
 
-# declare variables
-nonPrimeSum = 0
-primeSet = set()
+    # declare variables
+    non_prime_sum = 3  # count 3 as it will not be tested in loop
 
-# main loop
-for i in range(limit):
-    if isPrimeList[i]:
-        isFactor = False
+    # main loop
+    for i, isprime in enumerate(is_prime_list):
+        if not isprime or i == 3:  # ignore if num is composite or 3
+            continue
 
-        for j in range(int(log2(i))+1): 
+        for j in range(1, int(log2(i)) + 1): 
             if pow(10, 10**j, i) == 1:  # check for prime factors of repunits
-                isFactor = True
-                primeSet.add(i)
                 break
 
-        if not isFactor or i == 3:
-            nonPrimeSum += i
+        else:  # if prime did not divide any repunits
+            non_prime_sum += i
 
-# print result
-print(f"Sum of all the primes that will never be a factor: {nonPrimeSum}")
+    # print result
+    print("Sum of all the primes that will never be a factor: "\
+        f"{non_prime_sum}")
+
+
+if __name__ == "__main__":
+    main()
