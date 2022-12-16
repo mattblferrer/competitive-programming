44a499bcd841e408240a6de835d06f1b6f133b52
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2022-12-16 20:15:35 +0800

    updated 142, 148, 160

--- 101-150/148.py
+++ 101-150/148.py
@@ -5,20 +5,21 @@ Then, it calculates the remaining rows by brute force.
 """
 
 
-# generate triangular number of certain index
-def triangular(ind):
-    return ind*(ind+1)//2
+def triangular(ind: int) -> int:
+    """generate triangular number of certain index"""
+    return ind * (ind + 1) // 2
 
 
-# converts number to base b
-def num_to_base(num, b):
+def num_to_base(num: int, b: int) -> int:
+    """converts number to base b"""
     while num > 0:
         num, d = divmod(num, b)
         yield d
 
 
-# adds 1 to base b array
-def base_add_one(num_arr, b):
+
+def base_add_one(num_arr: list[int], b: int) -> list[int]:
+    """adds 1 to base b array"""
     num_arr[0] += 1
 
     for i in range(len(num_arr)):
@@ -30,44 +31,49 @@ def base_add_one(num_arr, b):
             return num_arr
 
 
-# declare variables
-limit = 10**9
-row = 7
+def main():
+    # declare variables
+    limit = 10**9
+    row = 7
 
-limit_conversion = list(num_to_base(limit, row))  # convert to base 7
-highestPower = len(limit_conversion) - 1
+    limit_conversion = list(num_to_base(limit, row))  # convert to base 7
+    highest_power = len(limit_conversion) - 1
 
-totalNums = triangular(limit)
-divisible = 0
+    total_nums = triangular(limit)
+    divisible = 0
 
-# populate triangular count array
-count = 0  # running count
-triangularCount = [0]  # number of divisible nums for each fractal level
+    # populate triangular count array
+    count = 0  # running count
+    triangular_count = [0]  # number of divisible nums for each fractal level
 
-for i in range(1, highestPower):
-    count += triangular(6) * triangular(7 ** i - 1)  # biggest triangles
-    count += (triangular(7) - 1) * triangularCount[i - 1]  # smaller triangles
-    triangularCount.append(count)
+    for i in range(1, highest_power):
+        count += triangular(6) * triangular(7 ** i - 1)  # biggest triangles
+        count += (triangular(7) - 1) * triangular_count[i - 1]  # smaller 
+        triangular_count.append(count)
 
-# calculate divisible with whole triangles
-n = limit_conversion[-1]
+    # calculate divisible with whole triangles
+    n = limit_conversion[-1]
 
-divisible += triangular(n) * triangularCount[-1]  # smaller triangles
-divisible += triangular(n-1) * triangular(7 ** (highestPower) - 1)
+    divisible += triangular(n) * triangular_count[-1]  # smaller triangles
+    divisible += triangular(n-1) * triangular(7 ** (highest_power) - 1)
 
-# calculate divisible with leftover partial triangles (brute force)
-row = n * 7 ** highestPower
-conv = list(num_to_base(row, 7))  # base 7 conversion of row number
+    # calculate divisible with leftover partial triangles (brute force)
+    row = n * 7 ** highest_power
+    conv = list(num_to_base(row, 7))  # base 7 conversion of row number
 
-for i in range(row+1, limit+1):
-    divRow = 1  # number of divisible numbers in row
+    for i in range(row + 1, limit + 1):
+        div_row = 1  # number of divisible numbers in row
 
-    for j in conv:  # calculate divisible nums 
-        divRow *= j+1
-        
-    divisible += i - divRow
-    conv = base_add_one(conv, 7)
+        for j in conv:  # calculate divisible nums 
+            div_row *= j + 1
+            
+        divisible += i - div_row
+        conv = base_add_one(conv, 7)
 
-# print result
-notDivisible = totalNums - divisible
-print("Rows not divisible by 7:", notDivisible)
+    # print result
+    not_divisible = total_nums - divisible
+    print("Rows not divisible by 7:", not_divisible)
+
+
+if __name__ == "__main__":
+    main()
--- 151-200/160.py
+++ 151-200/160.py
@@ -7,8 +7,8 @@ This is slightly modified to lower the iteration limit 10^8 to 1.5625*10^6.
 from math import log, prod
 
 
-# generates a list of numbers divisible by 2 and 5 below n (unsorted)
 def hamming(n: int) -> list[int]:
+    """generates a list of numbers divisible by 2 and 5 below n (unsorted)"""
     hamming_list = []
     mult = [2, 5]  # multipliers
     exps = [0, 0]  # prime exponents
@@ -29,17 +29,24 @@ def hamming(n: int) -> list[int]:
     return hamming_list
 
 
-# counts how many times the factor f appears in the prime factorizations of
-# 1 to n, including repeats
 def count_range_sum(f: int, n: int) -> int:
+    """counts how many times the factor f appears in the prime factorizations of
+    1 to n, including repeats
+    
+    parameters: 
+    f -- factor to be counted
+    n -- limit of prime factorizations to be calculated"""
     highest_power = int(log(n, f))
     count = sum(n // (f ** power) for power in range(1, highest_power+1))
 
     return count
 
 
-# creates a Sieve of Eratosthenes array of size n, coprime to 2 and 5
 def coprime_25(n: int) -> list[int]:
+    """creates a Sieve of Eratosthenes array of size n, coprime to 2 and 5
+    
+    parameters:
+    n -- size of sieve"""
     coprime_list = []
 
     for multiple in range(0, n, 10):
@@ -49,41 +56,46 @@ def coprime_25(n: int) -> list[int]:
     return coprime_list
 
 
-# declare variables
-n = 10**12
-digits = 5
-mod = 10**digits
-iterlimit = 15625*10**2  # cycle repeats every 1.5625*10^6
+def main():
+    # declare variables
+    n = 10**12
+    digits = 5
+    mod = 10**digits
+    iterlimit = 15625*10**2  # cycle repeats every 1.5625*10^6
 
-# get numbers below 10^8 coprime to 2 and 5, since 2*5 = 10 and does not count
-# towards the non-zero digits
-coprimeList = coprime_25(iterlimit + 1)
+    # get numbers below 10^8 coprime to 2 and 5, since 2*5 = 10 and does not 
+    # count towards the non-zero digits
+    coprime_list = coprime_25(iterlimit + 1)
 
-# calculate factor of 2 with modulo to be multiplied
-factors10 = count_range_sum(2, n) - count_range_sum(5, n)
-multiplier2 = pow(2, factors10, mod)
+    # calculate factor of 2 with modulo to be multiplied
+    factors_10 = count_range_sum(2, n) - count_range_sum(5, n)
+    multiplier_2 = pow(2, factors_10, mod)
 
-# calculate product of coprime numbers up to (including) n // hamming numbers
-hammingList = reversed(hamming(n))
-coprimeIndex = 1
-productDict = {}
-product = 1
+    # calculate product of coprime numbers up to (including) n
+    hamming_list = reversed(hamming(n))
+    coprime_index = 1
+    product_dict = {}
+    product = 1
 
-# memoization of product of coprime numbers up to and including n
-for i in range(1, iterlimit+1):
-    if coprimeList[coprimeIndex] == i:
-        product = (product * i) % (iterlimit)
-        coprimeIndex += 1
-    productDict[i] = product
+    # memoization of product of coprime numbers up to and including n
+    for i in range(1, iterlimit + 1):
+        if coprime_list[coprime_index] == i:
+            product = (product * i) % (iterlimit)
+            coprime_index += 1
+        product_dict[i] = product
 
-# calculate final 5 digits
-answer = multiplier2
-for currentHamming in hammingList:
-    q = (n // currentHamming) % (iterlimit)
-    if q == 0:
-        q = mod
+    # calculate final 5 digits
+    answer = multiplier_2
+    for current_hamming in hamming_list:
+        q = (n // current_hamming) % (iterlimit)
+        if q == 0:
+            q = mod
 
-    answer = (answer * productDict[q]) % mod
+        answer = (answer * product_dict[q]) % mod
 
-# print result
-print(f"f(1,000,000,000,000) = {answer}")
+    # print result
+    print(f"f(1,000,000,000,000) = {answer}")
+
+
+if __name__ == "__main__":
+    main()
