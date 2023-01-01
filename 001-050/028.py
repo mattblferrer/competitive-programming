ecf3a6243e7f4365338068040f72851670237064
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2023-01-01 16:46:06 +0800

    updated 28, 39

--- 001-050/028.py
+++ 001-050/028.py
@@ -4,12 +4,17 @@ After the spiral hits a perfect square number n^2, the sequence of numbers start
 For example, after the number 9, the spiral hits 9+4, 9+2*4, 9+3*4, etc, until it hits the next square 25
 The size of the spiral is n^2
 """
+def main():
+    diagonal_sum = 1
+    spiral_size = 1001  # given parameter
 
-diagonalSum = 1
-spiralSize = 1001  # given parameter
+    for i in range((spiral_size - 1) // 2):  # "rings" around central 1
+        for j in range(4):  # for the 4 corners of the spiral
+            # start in the top right corner and go counterclockwise
+            diagonal_sum += (2 * i + 3) ** 2 - j * (2 * i + 2)  
 
-for i in range((spiralSize-1)//2):  # this is the number of "rings" around the central 1
-    for j in range(4):  # for the 4 corners of the spiral
-        diagonalSum += (2*i+3)**2 - j*(2*i+2)  # start in the top right corner and go counterclockwise
+    print(diagonal_sum)  # print result
 
-print(diagonalSum)
+
+if __name__ == "__main__":
+    main()
--- 001-050/039.py
+++ 001-050/039.py
@@ -1,20 +1,27 @@
 from math import isqrt
 
 
-sumABC = 1000  # given a+b+c = 1000
-solutions = [0]*(sumABC+1)
-maximumIndex = 0  # index where the number of solutions is maximized
+def main():
+    sum_abc = 1000  # given a+b+c = 1000
+    solutions = [0]*(sum_abc + 1)
 
-# generating a, b, c
-for a in range(1, sumABC//2):
-    for b in range(1, sumABC//2):
-        c2 = a*a + b*b
-        c = isqrt(c2)
-        if c*c == c2 and a+b+c <= sumABC:  # check for pythagorean triple
-            solutions[a+b+c] += 1
+    # generating a, b, c
+    for a in range(1, sum_abc // 2):
+        for b in range(1, sum_abc // 2):
+            c2 = a*a + b*b
+            c = isqrt(c2)
+            if a + b + c > sum_abc:
+                break
+            if c*c == c2:  # check for pythagorean triple
+                solutions[a + b + c] += 1
 
-# finding maximum value p
-maximumIndex = solutions.index(max(solutions))
+    # finding maximum value p
+    maximum_index = solutions.index(max(solutions))
 
-# final output
-print(f"The value of p where the number of solutions is maximized is {maximumIndex}")
+    # final output
+    print("The value of p where the number of solutions is maximized"\
+        f" is {maximum_index}")
+
+
+if __name__ == "__main__":
+    main()
