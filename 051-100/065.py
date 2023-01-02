0b5b77e12d4f161e51b29729c92a7fda52b4ac69
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2023-01-03 01:02:03 +0800

    updated 65

--- 051-100/065.py
+++ 051-100/065.py
@@ -1,28 +1,24 @@
-# returns the term needed for the continued fraction of e
-def e_term(term):
-    if term % 3 == 2:
-        return ((term - 2)//3+1)*2
-    else:
-        return 1
+def e_term(n: int) -> int:
+    """returns the term needed for the n'th term of continued fraction of e"""
+    return ((n - 2) // 3 + 1) * 2 if n % 3 == 2 else 1
 
 
-# returns the nth convergent of e
-def e_convergent(term):
-    term -= 1  # offset
+def e_convergent(n: int) -> tuple[int]:
+    """returns the nth convergent of e"""
+    n -= 1  # offset
     numerator = 1
-    denominator = e_term(term)
+    denominator = e_term(n)
 
     # if 1st term
-    if term == 0:
+    if n == 0:
         return 2, 1
 
     # iterate upwards in the continued fraction
-    for i in range(term-1, 0, -1):
+    for i in range(n - 1, 0, -1):
         frac_term = e_term(i)
 
         # add current fraction to next term in continued fraction
         numerator += frac_term*denominator
-
         numerator, denominator = denominator, numerator  # reciprocal
 
     # add 2 to final fraction
@@ -30,8 +26,14 @@ def e_convergent(term):
     return numerator, denominator
 
 
-# declare variables
-sumOfDigits = sum(int(digit) for digit in str(e_convergent(100)[0]))
+def main():
+    # declare variables
+    sum_of_digits = sum(int(digit) for digit in str(e_convergent(100)[0]))
 
-# print output
-print("The sum of the digits of the 100th convergent of e is", sumOfDigits)
+    # print output
+    print("The sum of the digits of the 100th convergent of e is " \
+        f"{sum_of_digits}")
+
+
+if __name__ == "__main__":
+    main()
