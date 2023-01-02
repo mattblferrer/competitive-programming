2494c216bcf2268190939c8634edd70e9403cb3a
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2023-01-03 00:45:42 +0800

    updated 52

--- 051-100/052.py
+++ 051-100/052.py
@@ -1,23 +1,18 @@
-for i in range(100000, 1000000):  # number has to have at least 6 digits
-    # digits in i
-    temp = i
-    digits = set()
-    while i > 0:
-        digits.add(i % 10)
-        i //= 10
+def main(): 
+    for i in range(100_000, 1_000_000):  # number has to have at least 6 digits
+        digits = {int(digit) for digit in str(i)}  # digits in i
+        total_used_digits = set()  # used digits across i, 2i, 3i, 4i, 5i, 6i
 
-    # used digits across i, 2i, 3i, 4i, 5i, 6i
-    totalUsedDigits = set()
-    for j in range(1, 7):
-        mult = temp*j  # multiples of i
-        while mult > 0:
-            totalUsedDigits.add(mult % 10)
-            mult //= 10
+        for multiple in range(i, 7 * i, i):
+            total_used_digits.update({int(digit) for digit in str(multiple)})
+            if len(total_used_digits) > 6:  # no point to check further
+                break
 
-        if len(totalUsedDigits) > 6:  # no point to check further
+        # check if digits used are the same
+        if len(digits) == len(total_used_digits) == 6:
+            print(i)  # print answer
             break
 
-    # check if digits used are the same
-    if len(digits) == 6 and len(totalUsedDigits) == 6:
-        print(temp)  # print answer
-        break
+
+if __name__ == "__main__":
+    main()
