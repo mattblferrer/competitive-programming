52f8fc6a6b9e629f0393dcca4a6d4a1b456b0731
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2023-01-12 00:58:46 +0800

    solved 323

--- 301-350/323.py
+++ 301-350/323.py
@@ -1,4 +1,4 @@
-'''
+"""
 CODE FOR CHECKING:
 
 from random import getrandbits
@@ -13,4 +13,21 @@ for i in range(n):
         ctr += 1
 
 print(ctr / n)
-'''
+"""
+from fractions import Fraction
+
+
+def main():
+    # start with fraction for infinite precision calculation
+    sum_value = Fraction(1, 1)
+
+    # sum expected values for each turn
+    for i in range(1, 1000):
+        sum_value += (Fraction(1, 1) - (Fraction(2 ** i - 1, 2 ** i)) ** 32)
+
+    # print result
+    print(float(sum_value))
+
+
+if __name__ == "__main__":
+    main()
