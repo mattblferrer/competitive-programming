1bc759ec26e8193df632b00fd7d989eb90ea59ec
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-10-01 23:33:59 +0800

    solved 749A, 1368A, 1907A

---
+++ 749A.py
@@ -0,0 +1,10 @@
+def problem749A() -> None:
+    n = int(input())
+    print(n // 2)
+    if n % 2 == 0:
+        print("2 " * (n // 2))
+    else:
+        print("2 " * (n // 2 - 1) + "3")
+
+
+problem749A()
