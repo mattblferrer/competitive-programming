bba10ee074f89d0298595468c8bd666893f032b6
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-10-23 15:56:49 +0800

    organized 102697040

--- 102697040.py
+++ 102697040.py
@@ -1,40 +1,48 @@
 def problem102697040() -> str:
+    def valid_rows() -> bool:
+        # check if no number appears in same row twice
+        for row in range(9):
+            seen = [False] * 9
+            for col in range(9):  
+                if seen[sudoku[row][col] - 1]:
+                    return False
+                seen[sudoku[row][col] - 1] = True
+        return True
+
+    def valid_columns() -> bool:
+        # check if no number appears in same column twice
+        for col in range(9):
+            seen = [False] * 9
+            for row in range(9):
+                if seen[sudoku[row][col] - 1]:
+                    return False
+                seen[sudoku[row][col] - 1] = True
+        return True
+
+    def valid_boxes() -> bool:
+        # check if no number appears in same 3x3 square twice
+        for square_row in range(3):  # row of 3x3 square in grid
+            for square_col in range(3):  # column of 3x3 square in grid
+                seen = [False] * 9
+
+                for in_row in range(3):  # row inside square
+                    for in_col in range(3):  # column inside square
+                        row = square_row * 3 + in_row
+                        col = square_col * 3 + in_col
+                        
+                        if seen[sudoku[row][col] - 1]:
+                            return False
+                        seen[sudoku[row][col] - 1] = True
+        return True
+
     # get input sudoku as 9x9 array
     sudoku = []
     for _ in range(9):
         row = list(map(int, input().split(' ')))
-        seen = [False] * 9
-
-        # check if no number appears in same row twice
-        for n in row:  
-            if seen[n - 1]:
-                return "INVALID"
-            seen[n - 1] = True
         sudoku.append(row)
 
-    # check if no number appears in same column twice
-    for col in range(9):
-        seen = [False] * 9
-        for row in range(9):
-            if seen[sudoku[row][col] - 1]:
-                return "INVALID"
-            seen[sudoku[row][col] - 1] = True
-
-    # check if no number appears in same 3x3 square twice
-    for square_row in range(3):  # row of 3x3 square in grid
-        for square_col in range(3):  # column of 3x3 square in grid
-            seen = [False] * 9
-
-            for in_row in range(3):  # row inside square
-                for in_col in range(3):  # column inside square
-                    row = square_row * 3 + in_row
-                    col = square_col * 3 + in_col
-                    
-                    if seen[sudoku[row][col] - 1]:
-                        return "INVALID"
-                    seen[sudoku[row][col] - 1] = True
-            
-    return "VALID"
-    
+    if valid_rows() and valid_columns and valid_boxes():
+        return "VALID"
+    return "INVALID"
 
 print(problem102697040())
