b9d03a44ca5a798377379537377df3935c412f10
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-10-28 14:50:30 +0800

    fixed ABC180C

--- ABC180C.py
+++ ABC180C.py
@@ -1,15 +1,10 @@
 def ABC180C() -> list[int]:
     n = int(input())
-    factors = []
-    for i in range(1, int(n ** 0.5) + 1):
+    factors = set([1, n])
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
-            factors.append(i)
-            factors.append(n // i)
-            while n % i == 0:
-                n //= i
-
-        if i * i > n:
-            break
+            factors.add(i)
+            factors.add(n // i)
 
     return sorted(factors)
 
