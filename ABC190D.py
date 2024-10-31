05cff980340f4eb7d4f9e3b6de1263a2fb7df331
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-10-31 13:08:37 +0800

    optimized ABC190D

--- ABC190D.py
+++ ABC190D.py
@@ -3,11 +3,8 @@ def ABC190D() -> int:
     while n % 2 == 0:
         n //= 2
 
-    factors = 1  # including 1
-    if n != 1:
-        factors += 1
-
-    for i in range(2, int(n ** 0.5) + 1):
+    factors = 0
+    for i in range(1, int(n ** 0.5) + 1):
         if n % i == 0:
             factors += 1
             if i != (n // i):
