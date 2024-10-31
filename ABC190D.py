84a9a5820abfa3f479d7d41cad021a2130723a20
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-10-31 13:06:06 +0800

    optimized ABC190D

--- ABC190D.py
+++ ABC190D.py
@@ -1,16 +1,18 @@
 def ABC190D() -> int:
     n = int(input())
-    odd_factors = 1  # including 1
-    if n != 1 and n % 2 == 1:  # including itself if odd
-        odd_factors += 1
+    while n % 2 == 0:
+        n //= 2
+
+    factors = 1  # including 1
+    if n != 1:
+        factors += 1
 
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
-            if i % 2 == 1:
-                odd_factors += 1
-            if i != (n // i) and (n // i) % 2 == 1:
-                odd_factors += 1
+            factors += 1
+            if i != (n // i):
+                factors += 1
     
-    return odd_factors * 2
+    return factors * 2
 
 print(ABC190D())
