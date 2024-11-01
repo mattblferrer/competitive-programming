0a5be84c02ef09a6aa225762a2e0deb2918905fe
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-11-01 18:18:11 +0800

    optimized 1986A, 2014B

--- 1986A.py
+++ 1986A.py
@@ -1,13 +1,6 @@
 def problem1986A() -> int:
     points = list(map(int, input().split(" ")))
-    min_distance = float('inf')
-
-    for i in range(1, 11):  # check distances of all points in [1, 10]
-        distance = sum(abs(p - i) for p in points)
-        min_distance = min(min_distance, distance)
-
-    return min_distance
-
+    return max(points) - min(points)
 
 test_cases = int(input())
 for _ in range(test_cases):
--- 2014B.py
+++ 2014B.py
@@ -1,20 +1,11 @@
 def problem2014B() -> str:
     n, k = map(int, input().split())
-    if k % 4 == 0:
-        return "YES"
-    elif k % 4 == 1:
-        if n % 2 == 0:
-            return "YES"
-        else:
-            return "NO"
-    elif k % 4 == 2:
-        return "NO"
-    elif k % 4 == 3:
-        if n % 2 == 0:
-            return "NO"
-        else:
-            return "YES"
+    leaves = (n * (n + 1)) // 2 - ((n - k) * (n - k + 1)) // 2
 
+    if leaves % 2 == 0:
+        return "YES"
+    return "NO"
+    
 test_cases = int(input())
 for _ in range(test_cases):
     print(problem2014B())
