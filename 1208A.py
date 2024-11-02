707de39deb9c63362163e5ece0e46ee3e1b622f1
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-11-02 21:00:25 +0800

    optimized 1208A, 1296B

--- 1208A.py
+++ 1208A.py
@@ -1,12 +1,7 @@
 def problem1208A() -> int:
     a, b, n = map(int, input().split(" "))
-    if n % 3 == 0:
-        return a
-    if n % 3 == 1:
-        return b
-    if n % 3 == 2:
-        return a ^ b
-
+    seq = [a, b, a ^ b]
+    return seq[n % 3]
 
 test_cases = int(input())
 for _ in range(test_cases):
