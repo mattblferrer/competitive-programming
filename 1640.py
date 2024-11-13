9dbf5969fda4a96f6c03956fecc6fa56eaee23ee
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-11-14 02:19:31 +0800

    optimized 1640 with two pointer

--- 1640.py
+++ 1640.py
@@ -1,27 +1,16 @@
 def CSES1640() -> str:
     n, x = map(int, input().split())
-    a = list(map(int, input().split()))
-    sorted_a = sorted(a)
+    a = sorted((a_i, i) for i, a_i in enumerate(map(int, input().split())
+                                                ,start=1))
+    left, right = 0, n - 1
 
-    for i, a_i in enumerate(sorted_a):
-        if a_i >= x:
-            return "IMPOSSIBLE"
-        
-        left, right = -1, n
-        while right - left > 1:
-            mid = (left + right) // 2
-            if x - a_i < sorted_a[mid]:
-                right = mid
-            else:
-                left = mid
-                
-        n_1, n_2 = a_i, sorted_a[left]
-        if i == left:
-            continue
-        if n_1 + n_2 != x:
-            continue
-        
-        return f"{a.index(n_1) + 1} {n - a[::-1].index(n_2)}"
+    while left < right:
+        if a[left][0] + a[right][0] > x:
+            right -= 1
+        elif a[left][0] + a[right][0] < x:
+            left += 1
+        else:
+            return f"{a[left][1]} {a[right][1]}"
         
     return "IMPOSSIBLE"
 
