e373cb41695de1583cc34637dda89b5cabff712a
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-11-15 22:31:57 +0800

    solved 2031A, 2031B

---
+++ 2031B.py
@@ -0,0 +1,19 @@
+def problem2031B() -> str:
+    n = int(input())
+    p = list(map(int, input().split(' ')))
+    swapped = False
+
+    while not swapped:
+        swapped = True
+        for i in range(n - 1):
+            if p[i] == p[i + 1] + 1:
+                p[i], p[i + 1] = p[i + 1], p[i]
+                swapped = False
+                
+    if p == list(range(1, n + 1)):
+        return "YES"
+    return "NO"
+
+test_cases = int(input())
+for _ in range(test_cases):
+    print(problem2031B())
