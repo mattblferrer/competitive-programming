3005d90f35f0d8fcd48a936e730de92edd24c8b2
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-12-01 17:02:04 +0800

    added TODO 2039B

---
+++ 2039B.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+std::string problem2039B() {
+    std::string p;
+    std::cin >> p;
+    // TODO:
+
+    return "-1";
+}
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        std::cout << problem2039B() << std::endl;
+    }
+
+    return 0;
+}
