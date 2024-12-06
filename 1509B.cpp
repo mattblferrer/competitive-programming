0f32c1a056ca73ca66680a7dce78cabd9b9462ea
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-12-06 22:48:07 +0800

    solved 1509A, 2050A, TODO 1509B

---
+++ 1509B.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+std::string problem1509B() {
+    int n; 
+    std::string s;
+    std::cin >> n >> s;
+    for (int i = 0; i < n; i++) {
+        // TODO: ... 
+    }
+
+    return "YES";
+}
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        std::cout << problem1509B() << std::endl;
+    }
+
+    return 0;
+}
