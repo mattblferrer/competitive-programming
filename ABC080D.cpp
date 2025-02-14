9ad183cc4b999d580af20f3c9ad4ea34a8dd251c
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-02-14 20:27:43 +0800

    solved ABC 080D

--- ABC080D.cpp
+++ ABC080D.cpp
@@ -9,8 +9,21 @@ int ABC080D() {
     for (int i = 0; i < n; i++) {
         std::cin >> s[i] >> t[i] >> c[i];
     }
-    // TODO: solve
-    
+    int* a = new int[200002];
+    for (int i = 0; i < 200002; i++) {
+        a[i] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        a[2*s[i] - 1]++;
+        a[2*t[i]]--;
+    }
+    int max = 0;
+    int sum = 0;
+    for (int i = 0; i < 200002; i++) {
+        sum += a[i];
+        max = std::max(max, sum);
+    }
+    return std::min(max, ch);
 }
 
 int main() {
