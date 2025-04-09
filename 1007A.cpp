7650ba183bea45c9abdd117b197c807dbbdb53b0
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-04-09 13:26:36 +0800

    solved 1007A

--- 1007A.cpp
+++ 1007A.cpp
@@ -1,16 +1,22 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <vector>
 
 long long problem1007A() {
     long long n;
     std::cin >> n;
     std::vector<long long> a(n);
+    std::map<long long, long long> count;
     for (int i = 0; i < n; i++) {
         std::cin >> a[i];
+        count[a[i]]++;
     }
-    std::sort(a.begin(), a.end());
-    // TODO: solve
+    long long max = 0;
+    for (const auto& pair: count) {
+        max = std::max(max, pair.second);
+    }
+    return n - max;
 }
 
 int main() {
