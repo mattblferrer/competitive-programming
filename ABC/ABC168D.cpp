38cc5a7034f37ac57c39287c06710ff04f0e67d6
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-04-23 00:15:14 +0800

    solved ABC168 D

--- ABC/ABC168D.cpp
+++ ABC/ABC168D.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 void ABC168D() {
     int n, m, a_i, b_i;
@@ -10,7 +9,39 @@ void ABC168D() {
         adj_list[a_i - 1].push_back(b_i - 1);
         adj_list[b_i - 1].push_back(a_i - 1);
     }
-    // TODO: solve
+    std::vector<int> signs(n);
+    std::queue<int> q;
+    std::vector<bool> seen(n);
+    q.push(0);
+    seen[0] = true;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int v: adj_list[u]) {
+            if (seen[v]) continue;
+            seen[v] = true;
+            signs[v] = u;
+            q.push(v);
+        }
+    }
+
+    bool way = true;
+    for (int i = 0; i < n; i++) {
+        if (!seen[i]) {
+            way = false;
+            break;
+        }
+    }
+    if (!way) {
+        std::cout << "No\n";
+        return;
+    }
+    else {
+        std::cout << "Yes\n";
+    }
+    for (int i = 1; i < n; i++) {   
+        std::cout << signs[i] + 1 << "\n";
+    }
 }
 
 int main() {
