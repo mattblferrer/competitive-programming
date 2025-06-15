1dbaafb3ca649e2f45238f866972f78621997987
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-06-15 21:42:26 +0800

    TODO ABC119C

---
+++ ABC/ABC119C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int ABC119C() {
+    int n, a, b, c, mp = 0;
+    std::cin >> n >> a >> b >> c;
+    std::vector<int> l(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> l[i];
+    }
+    // TODO: solve
+}
+
+int main() {
+    std::cout << ABC119C();
+    return 0;
+}
