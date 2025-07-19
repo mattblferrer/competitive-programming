89988d2f90e62d20942c68d3aa3bd9504cdc0959
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-07-19 10:11:43 +0800

    solved ABC119C

--- ABC/ABC119C.cpp
+++ ABC/ABC119C.cpp
@@ -1,13 +1,27 @@
 #include <bits/stdc++.h>
 
-int ABC119C() {
-    int n, a, b, c, mp = 0;
-    std::cin >> n >> a >> b >> c;
-    std::vector<int> l(n);
-    for (int i = 0; i < n; i++) {
-        std::cin >> l[i];
+int n, a, b, c, l_i;
+std::vector<int> l;
+int search(int curr, int x, int y, int z) {
+    if (curr == n) {
+        if (std::min({x, y, z}) <= 0) return 10000000;
+        return abs(a - x) + abs(b - y) + abs(c - z) - 30;
     }
-    // TODO: solve
+    return std::min({
+        search(curr + 1, x, y, z),
+        search(curr + 1, x + l[curr], y, z) + 10, 
+        search(curr + 1, x, y + l[curr], z) + 10,
+        search(curr + 1, x, y, z + l[curr]) + 10
+    });
+}
+
+int ABC119C() {
+    std::cin >> n >> a >> b >> c;
+    for (int i = 0; i < n; i++) {
+        std::cin >> l_i;
+        l.push_back(l_i);
+    }
+    return search(0, 0, 0, 0);
 }
 
 int main() {
