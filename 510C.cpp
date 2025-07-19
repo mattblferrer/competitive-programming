ad74736fb830d6e9062cfc0d99515de1cbb1d8ff
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-07-19 12:00:09 +0800

    510C change dfs to topological sort

--- 510C.cpp
+++ 510C.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
-void dfs(std::vector<std::vector<int>>& adj_list, std::vector<bool>& seen, 
-    std::vector<int>& stack, std::vector<bool>& curr, bool& cycle, int u) {
+void topo_sort(std::vector<std::vector<int>>& adj_list, std::vector<bool>& seen, 
+    std::vector<int>& order, std::vector<bool>& curr, bool& cycle, int u) {
     if (seen[u]) return;
     if (curr[u]) {
         cycle = true;
@@ -9,10 +9,10 @@ void dfs(std::vector<std::vector<int>>& adj_list, std::vector<bool>& seen,
     }
     curr[u] = true;
     for (int v: adj_list[u]) {
-        dfs(adj_list, seen, stack, curr, cycle, v);
+        topo_sort(adj_list, seen, order, curr, cycle, v);
     }
     seen[u] = true;
-    stack.push_back(u);
+    order.push_back(u);
 }
 
 void problem510C() {
@@ -30,7 +30,7 @@ void problem510C() {
     }
     std::vector<std::vector<int>> adj_list(26);
     std::vector<bool> seen(26);
-    std::vector<int> stack;
+    std::vector<int> order;
     bool cycle = false;
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < std::min(name[i].length(), name[i + 1].length()); j++) {
@@ -41,14 +41,14 @@ void problem510C() {
     }
     for (int i = 0; i < 26; i++) {
         std::vector<bool> curr(26);
-        dfs(adj_list, seen, stack, curr, cycle, i);
+        topo_sort(adj_list, seen, order, curr, cycle, i);
         if (cycle) {
             std::cout << "Impossible";
             return;
         }
     }
     for (int i = 0; i < 26; i++) {
-        std::cout << (char)(stack[i] + 97);
+        std::cout << (char)(order[i] + 97);
     }
 }
 
