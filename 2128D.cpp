bb52fb39a2c3ff6af353df0bcd4eda6e3063e96c
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-07-30 17:21:02 +0800

    solved 2128D

--- 2128D.cpp
+++ 2128D.cpp
@@ -8,7 +8,12 @@ long long problem2128D() {
         std::cin >> p[i];
     }
     long long ans = 0;
-    // TODO: continue
+    for (int i = 1; i <= n; i++) {
+        ans += i * (n - i + 1);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        if (p[i] < p[i + 1]) ans -= (i + 1) * (n - i - 1);
+    }
     return ans;
 }
 
