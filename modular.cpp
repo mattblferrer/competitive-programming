be8705d08d966e583f5b950a8db059bc83512f82
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-08-07 17:49:46 +0800

    add mod inverse for nonprime modulo

--- modular.cpp
+++ modular.cpp
@@ -12,6 +12,26 @@ long long binexp(long long a, long long b, long long mod) {
     return ans;
 }
 
+long long modinv_p(long long a, long long p) {
+    return binexp(a, p - 2, p);
+}
+
+long long extended_euclidean(long long a, long long b, long long& x, long long& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long gcd = extended_euclidean(b, a % b, y, x);
+    y -= (a / b) * x;
+    return gcd;
+}
+
 long long modinv(long long a, long long mod) {
-    return binexp(a, mod - 2, mod);
+    long long x, y;
+    long long gcd = extended_euclidean(a, mod, x, y);
+    if (gcd != 1) {
+        return -1;  // doesn't exist
+    }
+    return (x % mod + mod) % mod;
 }
