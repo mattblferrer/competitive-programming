22338323b670344ca8fecab8e8c44f33b08d39f9
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-08-27 11:38:05 +0800

    solved 2122C

--- 2122C.cpp
+++ 2122C.cpp
@@ -3,19 +3,30 @@
 void problem2122C() {
     int n, x_i, y_i;
     std::cin >> n;
-    std::vector<std::pair<int, int>> xsort(n), ysort(n);
+    std::vector<std::vector<int>> xsort(n);
     for (int i = 0; i < n; i++) {
         std::cin >> x_i >> y_i;
-        xsort[i] = {x_i, y_i};
-        ysort[i] = {x_i, y_i};
+        xsort[i] = {x_i, y_i, i + 1};
     }
     std::sort(xsort.begin(), xsort.end(), [](const auto& a, const auto& b) {
-        return a.first < b.first;
+        return a[0] < b[0];
     });
-    std::sort(ysort.begin(), ysort.end(), [](const auto& a, const auto& b) {
-        return a.second < b.second;
+    std::vector<std::vector<int>> low_x, high_x;
+    for (int i = 0; i < n / 2; i++) {
+        low_x.push_back(xsort[i]);
+    }
+    for (int i = n / 2; i < n; i++) {
+        high_x.push_back(xsort[i]);
+    }
+    std::sort(low_x.begin(), low_x.end(), [](const auto& a, const auto& b) {
+        return a[1] < b[1];
     });
-    // TODO: upsolve
+    std::sort(high_x.begin(), high_x.end(), [](const auto& a, const auto& b) {
+        return a[1] < b[1];
+    });
+    for (int i = 0; i < n / 2; i++) {
+        std::cout << low_x[i][2] << " " << high_x[n / 2 - i - 1][2] << "\n";
+    }
 }
 
 int main() {
