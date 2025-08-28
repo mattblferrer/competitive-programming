a3b5ab99014d43785299ba75da212520885d1168
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-08-28 14:11:51 +0800

    tested global vector with 102861H

--- 102861H.cpp
+++ 102861H.cpp
@@ -1,28 +1,31 @@
 #include <bits/stdc++.h>
 
+std::vector<long long> w;
+
 long long comb(long long n, long long k) {
     if (k < 0) return 0;
     if (k == 0) return 1;
     return (n * comb(n - 1, k - 1)) / k;
 }
 
-long long backtrack(long long x, long long k, long long max_w, std::vector<long long>& w) {
+long long backtrack(long long x, long long k, long long max_w) {
     long long n = w.size();
     if (x == n) return (k == 0);
-    if (w[x] > max_w) return backtrack(x + 1, k, max_w, w);
-    return backtrack(x + 1, k - 1, max_w - w[x], w) + comb(n - x - 1, k);
+    if (w[x] > max_w) return backtrack(x + 1, k, max_w);
+    return backtrack(x + 1, k - 1, max_w - w[x]) + comb(n - x - 1, k);
 }
 
 long long problem102861H() {
-    long long n, k, a, b, ans;
+    long long n, k, a, b, w_i, ans;
     std::cin >> n >> k;
-    std::vector<long long> w(n);
+    
     for (int i = 0; i < n; i++) {
-        std::cin >> w[i];
+        std::cin >> w_i;
+        w.push_back(w_i);
     }
     std::cin >> a >> b;
     std::sort(w.begin(), w.end(), std::greater<long long>());
-    ans = backtrack(0, k, b, w) - backtrack(0, k, a - 1, w);
+    ans = backtrack(0, k, b) - backtrack(0, k, a - 1);
     return ans;
 }
 
