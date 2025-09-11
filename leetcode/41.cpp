a254bd9649ab555bff762410127743df64d2e3b7
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-11 21:13:47 +0800

    solved 41

---
+++ leetcode/41.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> cnt(n + 1);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0 && nums[i] <= n) cnt[nums[i]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (cnt[i] == 0) return i; 
+        }
+        return n + 1;
+    }
+};
