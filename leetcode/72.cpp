da1fe601fd7c7e0d699c9fbc165acf8b65b3e322
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-11 20:50:54 +0800

    solved 72

---
+++ leetcode/72.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        std::vector<std::vector<int>> d(n + 1, std::vector<int>(m + 1));
+        for (int i = 0; i <= n; i++) {
+            d[i][0] = i;
+        }
+        for (int i = 0; i <= m; i++) {
+            d[0][i] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int curr = 1;
+                if (word1[i - 1] == word2[j - 1]) curr = 0;
+                d[i][j] = std::min({curr + d[i - 1][j - 1], 1 + d[i - 1][j], 1 + d[i][j - 1]});
+            }
+        }
+        return d[n][m];
+    }
+};
