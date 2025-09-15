55965bfc67ac64831d8b99f32780bd07a9f08fe4
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-15 10:11:14 +0000

    added template for new cpp file

---
+++ cpp_templates/new_file.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using pll = std::pair<long long, long long>;
+
+using namespace std;
+
+void solve() {
+    
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int t; std::cin >> t; while (t--) solve();
+    return 0;
+}
