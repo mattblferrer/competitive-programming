4fef43093709d080342434bd3495d2a86e651936
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-16 18:28:31 +0800

    tested A+B

---
+++ yosupo/many_aplusb.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using pll = std::pair<long long, long long>;
+
+using namespace std;
+
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+    cout << a + b << "\n";
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int t; std::cin >> t; while (t--) solve();
+    return 0;
+}
