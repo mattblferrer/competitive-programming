237f421740335d4c4771d4c2858b8237d6fafd1a
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-19 09:20:27 +0800

    addition and subtraction big int

--- cpp_templates/big_integer.cpp
+++ cpp_templates/big_integer.cpp
@@ -2,62 +2,78 @@
 
 class BigInt {
     public:
-    std::string digits;
-    bool sign = false;
+    std::vector<signed char> d;  // digits
 
     BigInt(long long a = 0) {
-        if (a < 0) {
-            sign = true;
-            a = -a;
-        }
         while (a > 0) {
-            digits.push_back('0' + a % 10);
+            d.push_back(a % 10);
             a /= 10;
         }
     }
     BigInt(std::string& s) {
-        digits = "";
+        std::vector<signed char> d;
         int n = s.size();
-        for (int i = n - 1; i >= 0; i--) digits.push_back(s[i]);    
+        for (int i = n - 1; i >= 0; i--) d.push_back(s[i] - '0');    
     }   
     BigInt(const char* s) {
-        digits = "";
+        std::vector<signed char> d;
         int n = strlen(s);
-        for (int i = n - 1; i >= 0; i--) digits.push_back(s[i]);    
+        for (int i = n - 1; i >= 0; i--) d.push_back(s[i] - '0');    
     }   
+    int size() const {
+        return d.size();
+    }
     
-    // TODO: addition and subtraction
     friend BigInt &operator+=(BigInt& a, const BigInt& b) {
-        
+        int n = std::max(a.size(), b.size());
+        while (a.size() != n + 1) a.d.push_back(0);
+        for (int i = 0; i < b.size(); i++) a.d[i] += b.d[i];
+        for (int i = 0; i < n; i++) {
+            if (a.d[i] > 9) {
+                a.d[i] -= 10;
+                a.d[i + 1]++;
+            }
+        }
+        while (a.d.back() == 0) a.d.pop_back();
+        return a;
     }
     friend BigInt &operator-=(BigInt& a, const BigInt& b) {
-        
+        int n = std::max(a.size(), b.size());
+        for (int i = 0; i < b.size(); i++) a.d[i] -= b.d[i];
+        for (int i = 0; i < n; i++) {
+            if (a.d[i] < 0) {
+                a.d[i] += 10;
+                a.d[i + 1]--;
+            }
+        }
+        while (a.d.back() == 0) a.d.pop_back();
+        return a;
     }
-    char &operator[](const int idx) {
-        return digits[digits.size() - idx - 1];
+    friend BigInt operator+(BigInt& a, const BigInt& b) {
+        BigInt temp = a;
+        temp += b;
+        return temp;
+    }
+    friend BigInt operator-(BigInt& a, const BigInt& b) {
+        BigInt temp = a;
+        temp -= b;
+        return temp;
+    }
+    char operator[](const int idx) const {
+        return d[d.size() - idx - 1];
     }
     friend std::istream &operator>>(std::istream& in, BigInt& a) {
         std::string s;
         in >> s;
         int n = s.size();
-        if (s[0] == '-') {
-            a.sign = true;
-            s.erase(s.begin());
-        }
-        for (int i = n - 1; i >= 0; i--) a.digits.push_back(s[i]);
+        for (int i = n - 1; i >= 0; i--) a.d.push_back(s[i]);
         return std::cin;
     }
     friend std::ostream &operator<<(std::ostream& out, const BigInt& a) {
-        if (a.sign) std::cout << '-';
-        int n = a.digits.size();
+        int n = a.d.size();
         for (int i = n - 1; i >= 0; i--) {
-            std::cout << a.digits[i];
+            std::cout << (int)a.d[i];
         }
         return std::cout;
     }
 };
-
-int main() {
-    BigInt a(-32159120512);
-    std::cout << a;
-}
