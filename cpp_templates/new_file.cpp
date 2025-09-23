67893fcd223e27bd4c5d8f1229b904aaae53ed5f
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-23 13:39:00 +0800

    added INF and long double to template

--- cpp_templates/new_file.cpp
+++ cpp_templates/new_file.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using ll = long long;
+using ld = long double;
 using pll = std::pair<long long, long long>;
 using namespace std;
 #define M_PI 3.14159265358979323846
+const long long INF = 2e18;
 
 void solve() {
     
