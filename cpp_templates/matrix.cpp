a28b04021842cfdf63350af5ad5303c4c095b75f
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-23 11:50:30 +0800

    boilerplate for matrix class

---
+++ cpp_templates/matrix.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+class Matrix {
+    public:
+    std::vector<std::vector<long long>> a;
+
+    Matrix(int n, int m) {
+        
+    }
+    Matrix(std::vector<std::vector<long long>> a) {
+        
+    }
+};
