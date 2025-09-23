e43cfcda83334b7c6c13019b86730a654fd05aef
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-23 14:12:17 +0800

    solved 1312B

---
+++ codeforces/1312B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+using pll = std::pair<long long, long long>;
+using namespace std;
+#define M_PI 3.14159265358979323846
+const long long INF = 2e18;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), std::greater<int>());
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    cout << "\n";
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << setprecision(20);
+
+    int t; std::cin >> t; while (t--) solve();
+    return 0;
+}
