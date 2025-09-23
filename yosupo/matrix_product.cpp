58322cdd51b74e497108b119cb1a096867a28a69
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-23 15:12:33 +0800

    tested matrix product template

--- yosupo/matrix_product.cpp
+++ yosupo/matrix_product.cpp
@@ -1,4 +1,11 @@
 #include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+using pll = std::pair<long long, long long>;
+using namespace std;
+#define M_PI 3.14159265358979323846
+const long long INF = 2e18;
+const long long MOD = 998244353;
 
 class Matrix {
     public:
@@ -44,4 +51,20 @@ Matrix multiply(Matrix a, Matrix b, long long mod) {
         }
     }
     return c;
+}
+
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    Matrix a = matrix_input(n, m), b = matrix_input(m, k);
+    cout << multiply(a, b, MOD);
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << setprecision(20);
+
+    solve();
+    return 0;
 }
