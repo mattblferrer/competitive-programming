ce64140e28defaaf10ec891caac790aa74de373a
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-25 08:17:07 +0000

    add 1118C-D2 boilerplate

--- codeforces/1118C.cpp
+++ codeforces/1118C.cpp
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+using pll = std::pair<long long, long long>;
+using namespace std;
+#define M_PI 3.14159265358979323846
+const long long INF = 2e18;
+const long long MOD = 1000000007;
 
-void problem1118C() {
-    int n;
-    std::cin >> n;
-    std::vector<int> a(n * n);
-    std::map<int, int> count;
+void solve() {
+    int n, a_i;
+    cin >> n;
+    map<int, int> cnt;
     for (int i = 0; i < n * n; i++) {
-        std::cin >> a[i];
-        count[a[i]]++;
+        cin >> a_i;
+        cnt[a_i]++;
     }
 
-    // TODO: solve
-}
+} 
 
 int main() {
-    problem1118C();
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << setprecision(20);
+
+    solve();
     return 0;
 }
