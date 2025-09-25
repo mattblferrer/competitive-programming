367e8de3716c1570086bea3babbc693f7b93cbd4
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-25 15:01:18 +0800

    draft DP O code

---
+++ atcoder/DP/DP_O.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+using pll = std::pair<long long, long long>;
+using namespace std;
+#define M_PI 3.14159265358979323846
+const long long INF = 2e18;
+const long long MOD = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vector<int>> a(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    // TODO: solve
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << setprecision(20);
+
+    solve();
+    return 0;
+}
