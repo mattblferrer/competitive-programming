c02081ad38ed1ea4c0dc4f84aa2eee882e592700
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-26 13:15:10 +0800

    solved 1096A

---
+++ 1096A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+using pll = std::pair<long long, long long>;
+using namespace std;
+#define M_PI 3.14159265358979323846
+const long long INF = 2e18;
+const long long MOD = 1000000007;
+
+void solve() {
+    int l, r;
+    cin >> l >> r;
+    cout << l << " " << 2 * l << "\n";
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << setprecision(20);
+
+    int t; std::cin >> t; while (t--) solve();
+    return 0;
+}
