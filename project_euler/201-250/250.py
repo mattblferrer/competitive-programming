7771b96dbe5d4088b254f4d8fc04ae7e04fd9101
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-28 19:54:29 +0800

    delete old unsolved problems

--- project_euler/201-250/250.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-
-# get counts of each occurrence of x^x mod 250
-remainders = [pow(i, i, 250) for i in range(1, 250251)]
-print(Counter(remainders))
-    
-# TODO calculate number of subsets
