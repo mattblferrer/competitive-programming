67a54df74e9f5831d9902a24a0eb8ab7f6a694b8
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-09-30 22:17:49 +0800

    solved 106084A

---
+++ codeforces/gym/106084A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+using pll = std::pair<long long, long long>;
+using namespace std;
+#define M_PI 3.14159265358979323846
+const long long INF = 2e18;
+const long long MOD = 1000000007;
+
+void solve() {
+    ll x, d;
+    cin >> x >> d;
+    if (x * 2 > d) cout << "take it";
+    else cout << "double it";
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << setprecision(20);
+
+    solve();
+    return 0;
+}
