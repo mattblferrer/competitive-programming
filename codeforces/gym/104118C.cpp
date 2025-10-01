128929cbfab505949513adbdb068cd2d7f71cb2c
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-10-02 02:19:46 +0800

    2022 ICPC Asia-Manila Regional Contest C

---
+++ codeforces/gym/104118C.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+using pll = std::pair<long long, long long>;
+using namespace std;
+#define M_PI 3.14159265358979323846
+const long long INF = 2e18;
+const long long MOD = 1000000007;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int x = 0; x < min(100, k); x++) {
+        map<int, int> freq;
+        for (int i = 0; i < n; i++) {
+            freq[a[i]]++;
+        }
+        for (int i = 0; i < n; i++) {
+            a[i] = freq[a[i]];
+        }
+    }
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    cout << "\n";
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << setprecision(20);
+
+    solve();
+    return 0;
+}
