ae2904989e62ff7b6199694008685337822dddc5
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-10-02 14:43:45 +0800

    fix is_prime function for n  = 2

--- cpp_templates/number_theory.cpp
+++ cpp_templates/number_theory.cpp
@@ -37,8 +37,9 @@ long long modinv(long long a, long long mod) {
 }
 
 bool is_prime(long long n) {
+    if (n == 2) return true;
     if (n % 2 == 0) return false;
-    for (long long i = 3; i <= sqrtl(n) + 1; i += 2) {
+    for (long long i = 3; i * i <= n; i += 2) {
         if (n % i == 0) return false;
     }
     return n > 1;
