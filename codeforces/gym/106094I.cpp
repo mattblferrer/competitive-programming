4d6a1270cf1ffbaace77b42fc5685a1e85b3c381
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-10-03 20:01:13 +0800

    SVU-HIAST CPC 2025 I

---
+++ codeforces/gym/106094I.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+using pll = std::pair<long long, long long>;
+using namespace std;
+#define M_PI 3.14159265358979323846
+const long long INF = 2e18;
+const long long MOD = 1000000007;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    if (n == k) cout << "-1\n";
+    else cout << k + 1 << '\n';
+} 
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout << setprecision(20);
+
+    int t; std::cin >> t; while (t--) solve();
+    return 0;
+}
