a1e74be5543a799479ff1fd858739198ec540378
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-10-12 23:16:40 +0800

    solved 100886G

--- codeforces/gym/100886G.cpp
+++ codeforces/gym/100886G.cpp
@@ -10,12 +10,71 @@ const ll MOD = 1000000007;
 ll dp[20][2][2][2];
 ll digit[20][2][2][2];
 string a, b;
+ll n;
+
+ll find(ll pos, ll lb, ll ub, ll lead) {
+    if (pos == n) return lead ? 1 : -1;
+    if (dp[pos][lb][ub][lead] != -1) return dp[pos][lb][ub][lead];
+    
+    ll low = lb ? a[pos] : 0;
+    ll high = ub ? b[pos] : 9;
+    ll bestd = -1;
+    ll prod = -1;
+
+    for (ll d = low; d <= high; d++) {
+        ll nlb = lb && (d == low);
+        ll nub = ub && (d == high);
+        ll nlead = lead || (d != 0);
+        ll sub = find(pos + 1, nlb, nub, nlead);
+        if (sub == -1) continue;
+        ll curr = sub;
+        if (nlead) {
+            curr = (lead ? d : (d == 0 ? 1 : d)) * sub;
+        }
+        if (curr > prod) {
+            prod = curr;
+            bestd = d;
+        }
+    }
+    digit[pos][lb][ub][lead] = bestd;
+    return dp[pos][lb][ub][lead] = prod;
+}
 
 void solve() {
     ll ai, bi;
     cin >> ai >> bi;
     a = to_string(ai), b = to_string(bi);
-    // TODO: solve
+    while (a.size() < b.size()) a = '0' + a;
+    n = b.size();
+    for (int i = 0; i < n; i++) {
+        a[i] -= '0';
+        b[i] -= '0';
+    }
+    memset(dp, -1, sizeof(dp));
+    memset(digit, -1, sizeof(digit));
+    find(0, 1, 1, 0);
+
+    string ans = "";
+    ll pos = 0, lb = 1, ub = 1, lead = 0;
+    while (pos < n) {
+        ll d = digit[pos][lb][ub][lead];
+        ll low = lb ? a[pos] : 0;
+        ll high = ub ? b[pos] : 9;
+        ll nlb = lb && (d == low);
+        ll nub = ub && (d == high);
+        ll nlead = lead || (d != 0);
+        ans.push_back(d + '0');
+        lb = nlb; 
+        ub = nub; 
+        lead = nlead; 
+        pos++;
+    }
+    lead = 1;
+    for (ll i = 0; i < n; i++) {
+        if (ans[i] != '0') lead = 0;
+        if (!lead) cout << ans[i];  
+    }
+    
 } 
 
 int main() {
