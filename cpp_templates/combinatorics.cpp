61c13464ec7077afccd04646f9e8bb7d912f30a9
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-10-16 12:19:28 +0800

    added new comb mod template, removed old helper function

--- cpp_templates/combinatorics.cpp
+++ cpp_templates/combinatorics.cpp
@@ -23,14 +23,6 @@ ll modinv_p(ll a, ll p) {
     return binexp(a, p - 2, p);
 }
 
-void factorial_vec(ll n, ll mod, vector<ll> arr) {
-    arr.resize(n + 1);
-    arr[0] = 1;
-    for (int i = 1; i <= n; i++) {
-        arr[i] = arr[i - 1] * i % mod;
-    }
-}
-
 // main functions
 ll comb(ll n, ll k) {
     if (k < 0) return 0;
@@ -38,12 +30,16 @@ ll comb(ll n, ll k) {
     return (n * comb(n - 1, k - 1)) / k;
 }
 
+ll comb_m(ll n, ll k, ll mod) {
+    if (k < 0) return 0;
+    if (k == 0) return 1;
+    ll num = 1, denom = 1;
+    for (ll i = n; i >= n - k + 1; i--) num = (num * i) % mod;
+    for (ll i = 1; i <= k; i++) denom = (denom * i) % mod;
+    return (num * modinv_p(denom, mod)) % mod;
+}
+
 ll factorial(ll n) {
     if (n == 0) return 0;
     return n * factorial(n - 1);
-}
-
-ll comb_m(ll n, ll k, ll p) {
-    // call factorial_vec(n, p, fac)
-    return fac[n] * modinv_p((fac[k] * fac[n - k]) % p, p) % p;
 }
