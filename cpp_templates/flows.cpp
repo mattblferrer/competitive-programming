8018d403ed47e3e2242ea8d296271b7e4d83b5a5
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-10-18 16:04:45 +0800

    reduced length of flows template

--- cpp_templates/flows.cpp
+++ cpp_templates/flows.cpp
@@ -7,7 +7,7 @@ using pll = pair<ll, ll>;
 const ll INF = 2e18;
 ll fs;  // flow size
 vector<set<ll>> adj;  // adjacency of every vertex i
-vector<vector<ll>> capacity;  // residual capacity
+vector<vector<ll>> cap;  // residual capacity
 vector<vector<ll>> fg;  // flow graph (actual)
 
 ll bfs(ll s, ll t, vector<ll>& parent) {
@@ -17,14 +17,14 @@ ll bfs(ll s, ll t, vector<ll>& parent) {
     q.push({s, INF});
 
     while (!q.empty()) { 
-        ll curr = q.front().first, flow = q.front().second;
+        ll curr = q.front().first, f = q.front().second;
         q.pop();
         for (ll next: adj[curr]) {
-            if (parent[next] == -1 && capacity[curr][next]) {
+            if (parent[next] == -1 && cap[curr][next]) {
                 parent[next] = curr;
-                ll new_flow = min(flow, capacity[curr][next]);
-                if (next == t) return new_flow;
-                q.push({next, new_flow});
+                ll nf = min(f, cap[curr][next]);
+                if (next == t) return nf;
+                q.push({next, nf});
             }
         }
     }
@@ -32,40 +32,40 @@ ll bfs(ll s, ll t, vector<ll>& parent) {
 }
 
 ll max_flow(ll s, ll t) {
-    ll flow = 0, new_flow;
+    ll f = 0, nf;
     vector<ll> parent(fs);
 
-    while ((new_flow = bfs(s, t, parent))) {
-        flow += new_flow;
+    while ((nf = bfs(s, t, parent))) {
+        f += nf;
         ll curr = t;
         while (curr != s) {
             ll prev = parent[curr];
-            capacity[prev][curr] -= new_flow;
-            capacity[curr][prev] += new_flow;
-            fg[prev][curr] += new_flow;
-            fg[curr][prev] -= new_flow;
+            cap[prev][curr] -= nf;
+            cap[curr][prev] += nf;
+            fg[prev][curr] += nf;
+            fg[curr][prev] -= nf;
             curr = prev;
         }
     }
-    return flow;
+    return f;
 }
 
 void connect_d(ll u, ll v, ll w) {  // directed
     adj[u].insert(v);
     adj[v].insert(u);
-    capacity[u][v] += w;
+    cap[u][v] += w;
 }
 
 void connect_u(ll u, ll v, ll w) {  // undirected
     adj[u].insert(v);
     adj[v].insert(u);
-    capacity[u][v] += w;
-    capacity[v][u] += w;
+    cap[u][v] += w;
+    cap[v][u] += w;
 }
 
-void resize_flow(ll size) {
-    fs = size;
-    adj.resize(size);
-    capacity.resize(size, vector<ll>(size));
-    fg.resize(size, vector<ll>(size));
+void resize_flow(ll sz) {
+    fs = sz;
+    adj.resize(sz);
+    cap.resize(sz, vector<ll>(sz));
+    fg.resize(sz, vector<ll>(sz));
 }
