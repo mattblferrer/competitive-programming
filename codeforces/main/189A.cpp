d00fdb93291d4162a5112242e0ca9c1564f8ccd8
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-10-18 14:15:33 +0800

    solved 189A

---
+++ codeforces/main/189A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n, a, b, c;
+    cin >> n >> a >> b >> c;
+    vector<ll> dp(n + 1, -INF);
+    dp[0] = 0;
+    for (ll i = 0; i <= n; i++) {
+        if (i >= a) dp[i] = max(dp[i], dp[i - a] + 1);
+        if (i >= b) dp[i] = max(dp[i], dp[i - b] + 1);
+        if (i >= c) dp[i] = max(dp[i], dp[i - c] + 1);
+        
+    }
+    cout << dp[n];
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
