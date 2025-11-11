a002c43c4c5fdb00e858e819704e9122ebb1a7af
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-12 03:18:14 +0800

    Discrete Logarithm as template in C++

--- cpp_templates/number_theory.cpp
+++ cpp_templates/number_theory.cpp
@@ -40,6 +40,37 @@ ll modinv(ll a, ll mod) {
     return (x % mod + mod) % mod;
 }
 
+ll discrete_log(ll a, ll b, ll m) {
+    a %= m, b %= m;
+    ll k = 1LL, add = 0LL, g;
+    while ((g = gcd(a, m)) > 1) {
+        if (b == k) return add;
+        if (b % g) return -1;
+        b /= g, m /= g, ++add;
+        k = (k * 1LL * a / g) % m;
+    }
+
+    ll n = sqrtl(m) + 1;
+    ll an = 1LL;
+    for (ll i = 0; i < n; ++i)
+        an = (an * 1LL * a) % m;
+
+    unordered_map<ll, ll> vals;
+    for (ll q = 0, cur = b; q <= n; q++) {
+        vals[cur] = q;
+        cur = (cur * 1LL * a) % m;
+    }
+
+    for (ll p = 1, cur = k; p <= n; p++) {
+        cur = (cur * 1LL * an) % m;
+        if (vals.count(cur)) {
+            ll ans = n * p - vals[cur] + add;
+            return ans;
+        }
+    }
+    return -1;
+}
+
 bool is_prime(ll n) {
     if (n == 2) return true;
     if (n % 2 == 0) return false;
