00cf1fb805a4e6638542202a1fe706a100ed7493
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-20 00:24:34 +0800

    solved 2166A-B

---
+++ codeforces/main/2166A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    ll ans = 0;
+    for (ll i = 0; i < n; i++) {
+        if (s[i] != s[n - 1]) ans++;
+    }
+    cout << ans << '\n';
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
