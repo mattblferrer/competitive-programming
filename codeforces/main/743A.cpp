d7c9c0e933ecd07e1e5413c2eaa36d19e4190e5f
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-20 10:45:09 +0800

    solved 743A, C

---
+++ codeforces/main/743A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n, a, b;
+    cin >> n >> a >> b;
+    a--; b--;
+    string s;
+    cin >> s;
+    if (s[a] != s[b]) cout << 1;
+    else cout << 0;
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
