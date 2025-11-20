b4229f00d1ca6e83035b0a36df91186d93051604
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-20 10:10:47 +0800

    solved 1554A

---
+++ codeforces/main/1554A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+    ll ans = 0;
+    for (ll i = 0; i < n - 1; i++) {
+        ans = max(ans, a[i] * a[i + 1]);
+    }
+    cout << ans << "\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
