b784b2259061bc03a290e3865f3643f22af25ef4
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-21 19:42:13 +0800

    solved 1095A-B

---
+++ codeforces/main/1095A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (ll i = 1; i*(i + 1)/2 <= n; i++) {
+        cout << s[i*(i + 1)/2 - 1];
+    }
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
