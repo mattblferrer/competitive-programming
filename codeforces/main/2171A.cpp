8c98dbf8af80b03dc09bce41f930104573699501
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-21 21:47:29 +0800

    solved 2171A

---
+++ codeforces/main/2171A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    if (n % 2 == 1) cout << "0\n";
+    else cout << n / 4 + 1 << "\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
