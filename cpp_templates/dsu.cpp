5b3f5356bea513cef24c7ed8a184cff1d09f086d
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-24 23:22:45 +0800

    turn dsu, flows into struct

--- cpp_templates/dsu.cpp
+++ cpp_templates/dsu.cpp
@@ -4,29 +4,31 @@ using ll = long long;
 using ld = long double;
 using pll = pair<ll, ll>;
 
-vector<ll> parent;
-vector<ll> sz;
+struct dsu {
+    vector<ll> parent;
+    vector<ll> sz;
 
-void make_set(ll a) {
-    parent[a] = a;
-    sz[a] = 1;
-}
+    void make_set(ll a) {
+        parent[a] = a;
+        sz[a] = 1;
+    }
 
-ll find_set(ll a) {
-    if (parent[a] == a) return a;
-    return parent[a] = find_set(parent[a]);
-}
+    ll find_set(ll a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find_set(parent[a]);
+    }
 
-bool union_sets(ll a, ll b) {
-    a = find_set(a);
-    b = find_set(b);
-    if (a == b) return false;
-    if (sz[a] < sz[b]) swap(a, b);
-    parent[b] = a;
-    sz[a] += sz[b];
-    return true;
-}
+    bool union_sets(ll a, ll b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a == b) return false;
+        if (sz[a] < sz[b]) swap(a, b);
+        parent[b] = a;
+        sz[a] += sz[b];
+        return true;
+    }
 
-bool is_connected(ll a, ll b) {
-    return find_set(a) == find_set(b);
-}
+    bool is_connected(ll a, ll b) {
+        return find_set(a) == find_set(b);
+    }
+};
--- cpp_templates/flows.cpp
+++ cpp_templates/flows.cpp
@@ -3,69 +3,71 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 using pll = pair<ll, ll>;
-
 const ll INF = 2e18;
-ll fs;  // flow size
-vector<set<ll>> adj;  // adjacency of every vertex i
-vector<vector<ll>> cap;  // residual capacity
-vector<vector<ll>> fg;  // flow graph (actual)
 
-ll bfs(ll s, ll t, vector<ll>& parent) {
-    fill(parent.begin(), parent.end(), -1);
-    parent[s] = s;
-    queue<pll> q;
-    q.push({s, INF});
+struct flow {
+    ll fs;  // flow size
+    vector<set<ll>> adj;  // adjacency of every vertex i
+    vector<vector<ll>> cap;  // residual capacity
+    vector<vector<ll>> fg;  // flow graph (actual)
 
-    while (!q.empty()) { 
-        ll curr = q.front().first, f = q.front().second;
-        q.pop();
-        for (ll next: adj[curr]) {
-            if (parent[next] == -1 && cap[curr][next]) {
-                parent[next] = curr;
-                ll nf = min(f, cap[curr][next]);
-                if (next == t) return nf;
-                q.push({next, nf});
+    ll bfs(ll s, ll t, vector<ll>& parent) {
+        fill(parent.begin(), parent.end(), -1);
+        parent[s] = s;
+        queue<pll> q;
+        q.push({s, INF});
+
+        while (!q.empty()) { 
+            ll curr = q.front().first, f = q.front().second;
+            q.pop();
+            for (ll next: adj[curr]) {
+                if (parent[next] == -1 && cap[curr][next]) {
+                    parent[next] = curr;
+                    ll nf = min(f, cap[curr][next]);
+                    if (next == t) return nf;
+                    q.push({next, nf});
+                }
             }
         }
+        return 0;
     }
-    return 0;
-}
 
-ll max_flow(ll s, ll t) {
-    ll f = 0, nf;
-    vector<ll> parent(fs);
+    ll max_flow(ll s, ll t) {
+        ll f = 0, nf;
+        vector<ll> parent(fs);
 
-    while ((nf = bfs(s, t, parent))) {
-        f += nf;
-        ll curr = t;
-        while (curr != s) {
-            ll prev = parent[curr];
-            cap[prev][curr] -= nf;
-            cap[curr][prev] += nf;
-            fg[prev][curr] += nf;
-            fg[curr][prev] -= nf;
-            curr = prev;
+        while ((nf = bfs(s, t, parent))) {
+            f += nf;
+            ll curr = t;
+            while (curr != s) {
+                ll prev = parent[curr];
+                cap[prev][curr] -= nf;
+                cap[curr][prev] += nf;
+                fg[prev][curr] += nf;
+                fg[curr][prev] -= nf;
+                curr = prev;
+            }
         }
+        return f;
     }
-    return f;
-}
 
-void connect_d(ll u, ll v, ll w) {  // directed
-    adj[u].insert(v);
-    adj[v].insert(u);
-    cap[u][v] += w;
-}
+    void connect_d(ll u, ll v, ll w) {  // directed
+        adj[u].insert(v);
+        adj[v].insert(u);
+        cap[u][v] += w;
+    }
 
-void connect_u(ll u, ll v, ll w) {  // undirected
-    adj[u].insert(v);
-    adj[v].insert(u);
-    cap[u][v] += w;
-    cap[v][u] += w;
-}
+    void connect_u(ll u, ll v, ll w) {  // undirected
+        adj[u].insert(v);
+        adj[v].insert(u);
+        cap[u][v] += w;
+        cap[v][u] += w;
+    }
 
-void resize_flow(ll sz) {
-    fs = sz;
-    adj.resize(sz);
-    cap.resize(sz, vector<ll>(sz));
-    fg.resize(sz, vector<ll>(sz));
-}
+    void resize_flow(ll sz) {
+        fs = sz;
+        adj.resize(sz);
+        cap.resize(sz, vector<ll>(sz));
+        fg.resize(sz, vector<ll>(sz));
+    }
+};
