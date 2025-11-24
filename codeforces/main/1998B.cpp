2c5babc3b1b772fa6e93e5fde3e6a7e5f1227924
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-24 19:23:01 +0800

    solved 1998B

---
+++ codeforces/main/1998B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+    for (ll i = 1; i < n; i++) cout << a[i] << " ";
+    cout << a[0] << "\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
