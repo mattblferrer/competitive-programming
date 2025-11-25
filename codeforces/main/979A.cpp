60cdd5d8c741a8b92921cfa8795bab713a3bf104
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-25 14:14:41 +0800

    solved 805A, 979A

---
+++ codeforces/main/979A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    if (n == 0) cout << 0;
+    else if (n % 2 == 1) cout << (n + 1) / 2;
+    else cout << n + 1;
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
