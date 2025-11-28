edb1af69a5af8dda1e86784513e00a0e49ba1db0
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-11-29 02:06:56 +0800

    solved 1807C

---
+++ codeforces/main/1807C.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    map<char, set<ll>> par;
+    for (ll i = 0; i < n; i++) par[s[i]].insert(i % 2);
+    for (const auto& pair: par) {
+        if (pair.second.size() == 2) {
+            cout << "NO\n";
+            return;
+        } 
+    }
+    cout << "YES\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
