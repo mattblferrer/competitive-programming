2bca410d26e42edea198e8f996b40275bd4b35f5
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-12-16 16:06:31 +0800

    solved 412B

---
+++ codeforces/main/412B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+    sort(a.begin(), a.end(), greater<ll>());
+    cout << a[k - 1];
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
