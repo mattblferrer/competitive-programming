ee093cfae0ff515de3bb8ff80cc78c3db26575f5
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-12-16 15:59:43 +0800

    solved 513A

---
+++ codeforces/main/513A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n1, n2, k1, k2;
+    cin >> n1 >> n2 >> k1 >> k2;
+    if (n1 > n2) cout << "First\n";
+    else cout << "Second\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
