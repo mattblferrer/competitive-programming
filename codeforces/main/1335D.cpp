8fb5435a723fa5201838ce3f637f60b0beaff9d4
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2025-12-17 23:21:27 +0800

    solved 1335D

---
+++ codeforces/main/1335D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    vector<string> grid(9);
+    for (ll i = 0; i < 9; i++) cin >> grid[i];
+    for (ll i = 0; i < 9; i++) {
+        for (ll j = 0; j < 9; j++) {
+            if (grid[i][j] == '9') grid[i][j] = '1';
+        }
+    }
+    for (ll i = 0; i < 9; i++) cout << grid[i] << "\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
