2a4be772131bfcf0e7dc3435a896d94d6f0ccd76
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-01-10 20:57:03 +0800

    solved 1517A

---
+++ codeforces/main/1517A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    ll q = n / 2050;
+    if (n % 2050 != 0) {
+        cout << "-1\n";
+        return;
+    }
+    ll ans = 0;
+    while (q > 0) {
+        ans += q % 10;
+        q /= 10;
+    }
+    cout << ans << "\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
