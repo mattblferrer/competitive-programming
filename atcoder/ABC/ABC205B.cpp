8e5a6e9f6f7d1f8c826f8b42d2d3d92547899045
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-01-25 00:00:29 +0800

    solved ABC205A-B

---
+++ atcoder/ABC/ABC205B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    for (ll i = 0; i < n; i++) {
+        if (a[i] != i + 1) {
+            cout << "No\n";
+            return;
+        }
+    }
+    cout << "Yes\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
