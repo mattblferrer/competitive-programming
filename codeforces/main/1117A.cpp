3dcd0aa49124e6af97403e98802e96f7597c667a
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-03-17 13:04:42 +0800

    solved 1117A

---
+++ codeforces/main/1117A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+    ll m = 0;
+    for (ll i = 0; i < n; i++) {
+        m = max(m, a[i]);
+    }
+    ll curr = 0, ans = 0;
+    for (ll i = 0; i < n; i++) {
+        if (a[i] != m) curr = 0;
+        else curr++;
+        ans = max(ans, curr);
+    }
+    cout << ans << "\n";
+} 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
