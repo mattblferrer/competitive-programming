c896f92c9db99cd4f254e592b3149dc0646246e7
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-05-12 23:37:22 +0800

    LC 3370 change alias for uint

--- leetcode/3370.cpp
+++ leetcode/3370.cpp
@@ -4,6 +4,6 @@ using namespace std;
 class Solution {
 public:
     int smallestNumber(int n) {
-        return (1 << bit_width((uint)n)) - 1;
+        return (1 << bit_width((unsigned int)n)) - 1;
     }
 };
