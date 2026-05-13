f8e01e1e5d302b72843715ed92a4cd7545e5d50a
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-05-13 21:17:19 +0800

    LC 3765 formatting changes

--- leetcode/3765.cpp
+++ leetcode/3765.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
     bool isPrime(int num) {
-        for (int i = 2; i*i <= num; i++) {
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) return false;
         }
         return (num > 1);
