161efb5de50182ec4913cbd1a8d6f564a0e9597a
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-05-14 19:12:25 +0800

    solved LC 2784

---
+++ leetcode/2784.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] != i + 1) return false;
+        }
+        return nums.back() == nums.size() - 1;
+    }
+};
