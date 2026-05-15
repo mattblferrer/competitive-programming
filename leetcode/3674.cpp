c958fba1b48e6e78537bb1ad2a8f4f126fb957e0
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-05-15 23:22:59 +0800

    solved LC 3674

---
+++ leetcode/3674.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] != nums[i + 1]) return 1;
+        }
+        return 0;
+    }
+};
