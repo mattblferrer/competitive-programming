aa00f798afc7f1be8f31157b583c1cb7bc0612df
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-05-17 21:43:14 +0800

    solved LC 3898

---
+++ leetcode/3898.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j]) ans[i]++;
+            }
+        }
+        return ans;
+    }
+};
