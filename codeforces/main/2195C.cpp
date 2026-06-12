40ac08d71a98d5d996774ad935b33cd9c73ac86c
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-06-12 14:23:12 +0800

    solved CF 2195B-C

---
+++ codeforces/main/2195C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+    ll ans = 0;
+    for (ll i = 0; i < n - 1; i++) {
+        if ((a[i] + a[i + 1] == 7) || (a[i] == a[i + 1])) {
+            ans++;
+            i++;
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
