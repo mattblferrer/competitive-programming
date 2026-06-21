8398e3fee669dec918485b0900b69d5ae663c325
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-06-21 21:55:14 +0800

    add point update array based segment tree struct

--- cpp_templates/segment_tree.cpp
+++ cpp_templates/segment_tree.cpp
@@ -4,18 +4,42 @@ using ll = long long;
 using ld = long double;
 using pll = pair<ll, ll>;
 
-struct segtree {
+
+struct point_segtree {
+    int n;
+    ll *vals;
+    point_segtree(vector<ll> &ar, int n) {
+        this->n = n;
+        vals = new ll[2 * n];
+        for (int i = 0; i < n; ++i) vals[i + n] = ar[i];
+        for (int i = n - 1; i > 0; --i) vals[i] = vals[i << 1] + vals[i << 1 | 1];
+    }
+    void update(int i, ll v) {
+        for (vals[i += n] += v; i > 1; i >>= 1)
+            vals[i >> 1] = vals[i] + vals[i ^ 1];
+    }
+    ll query(int l, int r) {
+        ll res = 0;
+        for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) res += vals[l++];
+            if (r & 1) res += vals[--r];
+        }
+        return res;
+    }
+};
+
+struct range_segtree {
     ll i, j, val, temp_val = 0;
-    segtree *l, *r;
-    segtree(vector<ll> &ar, int _i, int _j): i(_i), j(_j) {
+    range_segtree *l, *r;
+    range_segtree(vector<ll> &ar, int _i, int _j): i(_i), j(_j) {
         if (i == j) {
             val = ar[i];
             l = r = NULL;
         }
         else {
             int k = (i + j) >> 1;
-            l = new segtree(ar, i, k);
-            r = new segtree(ar, k + 1, j);
+            l = new range_segtree(ar, i, k);
+            r = new range_segtree(ar, k + 1, j);
             val = l->val + r->val;
         }
     }
