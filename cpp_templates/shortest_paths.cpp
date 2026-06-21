828737251a7e0045b74f595b175463ef910336e4
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-06-21 16:47:07 +0800

    changed shortest path algorithm templates to structs

--- cpp_templates/shortest_paths.cpp
+++ cpp_templates/shortest_paths.cpp
@@ -5,61 +5,93 @@ using ld = long double;
 using pll = pair<ll, ll>;
 const ll INF = 2e18;
 
-void bellman_ford(ll start, ll n, vector<vector<ll>> edges,
-    vector<ll>& dist) {
-    dist.resize(n, INF);
-    dist[start] = 0;
-    for (ll i = 0; i < n - 1; i++) {
-        for (auto e : edges) {
-            dist[e[1]] = min(dist[e[1]], dist[e[0]] + e[2]);
-        }
-    }
-}
+struct bellman_ford {
+    ll start, n;
+    vector<vector<ll>> edges;
+    vector<ll> dist;
 
-vector<vector<ll>> floyd_warshall(vector<vector<ll>> adj) {
-    ll n = adj.size();
-    vector<vector<ll>> dist(n, vector<ll>(n, INF));
-    for (ll i = 0; i < n; i++) {
-        for (ll j = 0; j < n; j++) {
-            if (i == j) dist[i][j] = 0;
-            else if (adj[i][j]) dist[i][j] = adj[i][j];
+    bellman_ford(ll start, ll n, vector<vector<ll>> edges) {
+        this->start = start;
+        this->n = n;
+        this->edges = edges;
+        dist.resize(n, INF);
+        dist[start] = 0;
+    }
+
+    void run() {
+        for (ll i = 0; i < n - 1; i++) {
+            for (auto e : edges) {
+                dist[e[1]] = min(dist[e[1]], dist[e[0]] + e[2]);
+            }
         }
     }
-    for (ll k = 0; k < n; k++) {
+};
+
+struct floyd_warshall {
+    ll n;
+    vector<vector<ll>> adj, dist;
+
+    floyd_warshall(vector<vector<ll>> adj) {
+        this->adj = adj;
+        this->n = adj.size();
+        dist.resize(n, vector<ll>(n, INF));
+    }
+
+    void run() {
         for (ll i = 0; i < n; i++) {
             for (ll j = 0; j < n; j++) {
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                if (i == j) dist[i][j] = 0;
+                else if (adj[i][j]) dist[i][j] = adj[i][j];
+            }
+        }
+        for (ll k = 0; k < n; k++) {
+            for (ll i = 0; i < n; i++) {
+                for (ll j = 0; j < n; j++) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
             }
         }
     }
-    return dist;
-}
+};
 
-void dijkstra(ll start, vector<vector<pll>> adj_list,
-    vector<ll>& dist, vector<ll>& path) {
-    int n = adj_list.size();
-    dist.resize(n, INF);
-    path.resize(n, -1);
-    priority_queue<pll, vector<pll>, greater<pll>> q;
+struct dijkstra {
+    ll start, size;
+    vector<vector<pll>> adj_list;
+    vector<ll> dist, path;
 
-    dist[start] = 0;  // starting vertex
-    q.push({0, start});
-    while (!q.empty()) {
-        pll u = q.top();
-        ll d_f = u.first;
-        ll f = u.second;
-        q.pop();
+    dijkstra(ll start, vector<vector<pll>> adj_list) {
+        this->start = start;
+        this->adj_list = adj_list;
+        this->size = adj_list.size();
+        dist.resize(size, INF);
+        path.resize(size, -1);
+    }
 
-        if (d_f != dist[f]) continue;
-        for (auto edge : adj_list[f]) {
-            ll length = edge.first;
-            ll v = edge.second;
+    void run() {
+        priority_queue<pll, vector<pll>, greater<pll>> q;
+        dist[start] = 0;  // starting vertex
+        q.push({0, start});
+        while (!q.empty()) {
+            pll u = q.top();
+            ll d_f = u.first;
+            ll f = u.second;
+            q.pop();
 
-            if (dist[f] + length < dist[v]) {
-                dist[v] = dist[f] + length;
-                path[v] = f;
-                q.push({dist[v], v});
+            if (d_f != dist[f]) continue;
+            for (auto edge : adj_list[f]) {
+                ll length = edge.first;
+                ll v = edge.second;
+
+                if (dist[f] + length < dist[v]) {
+                    dist[v] = dist[f] + length;
+                    path[v] = f;
+                    q.push({dist[v], v});
+                }
             }
         }
     }
-}
+
+    bool has_path(ll end) {
+        return !(dist[end] >= INF);
+    }
+};
--- yosupo/shortest_path.cpp
+++ yosupo/shortest_path.cpp
@@ -5,34 +5,47 @@ using namespace std;
 
 const long long INF = 2e18;
 
-void dijkstra(ll start, std::vector<std::vector<pll>> adj_list, 
-    std::vector<ll>& dist, std::vector<ll>& path) {
-    int n = adj_list.size();
-    dist.resize(n, INF);
-    path.resize(n, -1);
-    std::priority_queue<pll, std::vector<pll>, std::greater<pll>> q;
+struct dijkstra {
+    ll start, size;
+    vector<vector<pll>> adj_list;
+    vector<ll> dist, path;
 
-    dist[start] = 0;  // starting vertex
-    q.push({0, start});
-    while (!q.empty()) {
-        pll u = q.top();
-        ll d_f = u.first;
-        ll f = u.second;
-        q.pop();
+    dijkstra(ll start, vector<vector<pll>> adj_list) {
+        this->start = start;
+        this->adj_list = adj_list;
+        this->size = adj_list.size();
+        dist.resize(size, INF);
+        path.resize(size, -1);
+    }
 
-        if (d_f != dist[f]) continue;
-        for (auto edge: adj_list[f]) {
-            ll length = edge.first;
-            ll v = edge.second;
-            
-            if (dist[f] + length < dist[v]) {
-                dist[v] = dist[f] + length;
-                path[v] = f;
-                q.push({dist[v], v});
+    void run() {
+        priority_queue<pll, vector<pll>, greater<pll>> q;
+        dist[start] = 0;  // starting vertex
+        q.push({0, start});
+        while (!q.empty()) {
+            pll u = q.top();
+            ll d_f = u.first;
+            ll f = u.second;
+            q.pop();
+
+            if (d_f != dist[f]) continue;
+            for (auto edge : adj_list[f]) {
+                ll length = edge.first;
+                ll v = edge.second;
+
+                if (dist[f] + length < dist[v]) {
+                    dist[v] = dist[f] + length;
+                    path[v] = f;
+                    q.push({dist[v], v});
+                }
             }
         }
     }
-}
+
+    bool has_path(ll end) {
+        return !(dist[end] >= INF);
+    }
+};
 
 void solve() {
     ll n, m, s, t, a, b, c;
@@ -42,25 +55,26 @@ void solve() {
         cin >> a >> b >> c;
         adj_list[a].push_back({c, b});
     }
-    vector<ll> dist, path;
-    dijkstra(s, adj_list, dist, path);
-    if (dist[t] == INF) {
+    dijkstra djk(s, adj_list);
+    djk.run();
+    if (!djk.has_path(t)) {
         cout << -1;
         return;
     }
+
     vector<ll> path_nodes;
     ll curr = t;
     while (curr != s) {
         path_nodes.push_back(curr);
-        curr = path[curr];
+        curr = djk.path[curr];
     }
     path_nodes.push_back(s);
     reverse(path_nodes.begin(), path_nodes.end());
-    cout << dist[t] << " " << path_nodes.size() - 1 << "\n";
+    cout << djk.dist[t] << " " << path_nodes.size() - 1 << "\n";
     for (int i = 0; i < path_nodes.size() - 1; i++) {
         cout << path_nodes[i] << " " << path_nodes[i + 1] << "\n";
     }
-} 
+}
 
 int main() {
     std::ios_base::sync_with_stdio(false);
