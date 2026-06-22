5b6565848eea57c8603b01b57842edfe9f21cff5
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-06-22 23:10:42 +0800

    added array based 2D segment tree template

--- cpp_templates/segment_tree.cpp
+++ cpp_templates/segment_tree.cpp
@@ -75,3 +75,42 @@ struct range_segtree {
         else return l->query(_i, _j) + r->query(_i, _j);
     }
 };
+
+struct segtree_2d {
+    int n, m;
+    ll **ar;
+    segtree_2d(int n, int m) {
+        this->n = n;
+        this->m = m;
+        ar = new ll * [2 * n];
+        for (int i = 0; i < 2 * n; ++i) {
+            ar[i] = new ll[2 * m];
+            for (int j = 0; j < 2 * m; ++j) ar[i][j] = 0;
+        }
+    }
+    void update(int r, int c, ll v) {
+        ar[r + n][c + m] = v;
+        for (int i = r + n; i > 0; i >>= 1) {
+            for (int j = c + m; j > 0; j >>= 1) {
+                ar[i >> 1][j] = ar[i][j] + ar[i ^ 1][j];
+                ar[i][j >> 1] = ar[i][j] + ar[i][j ^ 1];
+            }
+        }
+    }
+    ll query(int r1, int r2, int c1, int c2) {
+        ll s = 0;
+        if (~r2) {
+            for (int a = r1 + n, b = r2 + n + 1; a < b; a >>= 1, b >>= 1) {
+                if (a & 1) s += query(a++, -1, c1, c2);
+                if (b & 1) s += query(--b, -1, c1, c2);
+            }
+        }
+        else {
+            for (int a = c1 + m, b = c2 + m + 1; a < b; a >>= 1, b >>= 1) {
+                if (a & 1) s += ar[r1][a++];
+                if (b & 1) s += ar[r1][--b];
+            }
+        }
+        return s;
+    }
+};
