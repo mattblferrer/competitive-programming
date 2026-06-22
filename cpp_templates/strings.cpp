6702ace5393838857fc39329c209f54d08f510d1
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-06-22 23:43:55 +0800

    added KMP string algorithm template

---
+++ cpp_templates/strings.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+
+struct KMP {
+    vector<int> par;
+    int n;
+    string s, f;
+    KMP(string &s, string &f) {
+        this->n = s.size(); this->s = s; this->f = f;
+        if (f.size() > s.size()) return;
+
+        par.resize(max(n, 2), 0);
+        par[0] = -1, par[1] = 0;
+        int i = 2, j = 0;
+        while (i <= f.size()) {
+            if (f[i - 1] == f[j]) par[i++] = ++j;
+            else if (j > 0) j = par[j];
+            else par[i++] = 0;
+        }
+    }
+    vector<int> run() {
+        if (f.size() > s.size()) return {};
+
+        int i = 0, j = 0; vector<int> ans;
+        while (i + j < s.size()) {
+            if (s[i + j] == f[j]) {
+                if (++j == f.size()) {
+                    ans.push_back(i);
+                    i += j - par[j];
+                    if (j > 0) j = par[j];
+                }
+            }
+            else {
+                i += j - par[j];
+                if (j > 0) j = par[j];
+            }
+        }
+        return ans;
+    }
+};
