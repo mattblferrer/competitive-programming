afa0141738d0f383f541a61c76a3d260c921e9cc
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-07-18 21:36:29 +0800

    AtCoder Beginner Contest 467

---
+++ atcoder/ABC/ABC467A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ld h, w;
+    cin >> h >> w;
+    ld bmi = w / (h / 100) / (h / 100);
+    if (bmi >= 25) cout << "Yes\n";
+    else cout << "No\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
