0e69d4586cb8ae845754f02969a3bc0f3bbfd67f
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-08-13 03:04:42 +0800

    solved ARC 194A

---
+++ atcoder/ARC/ARC194A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n + 1);
+    for (ll i = 1; i <= n; i++) cin >> a[i];
+
+    vector<ll> dp(n + 1);
+    dp[1] = a[1];
+    for (ll i = 2; i <= n; i++) {
+        dp[i] = max(dp[i - 2], a[i] + dp[i - 1]);
+    }
+    cout << dp[n] << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
