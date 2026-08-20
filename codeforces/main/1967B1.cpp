51c4b200276236bd0f97cc662283d9bff1867ab4
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2026-08-20 19:17:03 +0800

    solved CF 1967B1

---
+++ codeforces/main/1967B1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1000000007;
+
+void solve() {
+    ll n, m;
+    cin >> n >> m;
+
+    ll ans = 0;
+    for (ll b = 1; b <= m; b++) {
+        ans += (n + b) / (b * b);
+    }
+
+    cout << ans - 1 << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    int t; cin >> t; while (t--) solve();
+    return 0;
+}
