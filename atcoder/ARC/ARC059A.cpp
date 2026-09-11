7d0dc08201e3ba1b3c088f4df8fcaeb585ee0230
Matt Ferrer <mattbrycen888@gmail.com>
2026-09-11 17:46:13 +0800

    AtCoder Beginner Contest 043

---
+++ atcoder/ARC/ARC059A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1'000'000'007;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    ll ans = INF;
+    for (ll k = -100; k <= 100; k++) {
+        ll curr = 0;
+        for (ll i = 0; i < n; i++) {
+            curr += (a[i] - k) * (a[i] - k);
+        }
+        ans = min(ans, curr);
+    }
+
+    cout << ans << "\n";
+}   
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
