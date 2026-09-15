09f7539b128d3a4a213575e2ee84b7f01e4a969c
Matt Ferrer <mattbrycen888@gmail.com>
2026-09-15 14:48:03 +0800

    solved ABC 170A-C

---
+++ atcoder/ABC/ABC170C.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+#define M_PI 3.14159265358979323846
+const ll INF = 2e18;
+const ll MOD = 1'000'000'007;
+
+void solve() {
+    ll x, n;
+    cin >> x >> n;
+    set<ll> p;
+    for (ll i = 0; i < n; i++) {
+        ll pi;
+        cin >> pi;
+        p.insert(pi);
+    }
+    for (ll diff = 0; diff <= 100; diff++) {
+        if (p.count(x - diff) == 0) {
+            cout << x - diff << "\n";
+            return;
+        }
+        if (p.count(x + diff) == 0) {
+            cout << x + diff << "\n";
+            return;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << setprecision(20);
+
+    solve();
+    return 0;
+}
